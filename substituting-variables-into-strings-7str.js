fdfcc591f211cb09a53339c5ae3f22b539d6b42f
Eric Carlson <eric.carlson2972@gmail.com>
2022-01-11 07:01:59 -0500

    modified structure

--- substituting-variables-into-strings-7str.js
+++ substituting-variables-into-strings-7str.js
@@ -3,6 +3,13 @@
 // Example:
 // solution(5) // should return "Value is 00005"
 
+//PREP
+//Parameter -
+//Return -
+//Example -
+//Pseudocode -
+//Methods!
+
 function solution(value) {
   return `Value is ${String(value).padStart(5, 0)}`;
 }
