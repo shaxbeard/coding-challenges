a2ae8cc36b1e9e21ff5ee3f7c89e98ee3b9a492f
Eric Carlson <eric.carlson2972@gmail.com>
2022-01-20 07:07:17 -0500

    modified structure

--- remove-anchor-from-url-7str.js
+++ remove-anchor-from-url-7str.js
@@ -3,8 +3,15 @@
 // "www.codewars.com#about" --> "www.codewars.com"
 // "www.codewars.com?page=1" -->"www.codewars.com?page=1"
 
-function removeAnchor(url) {
-  return url;
-}
+//PREP
+//parameters -> string, no int, float, no null, no undefined, no empty string
+//return - if the url has # -> remove the hash AND any characters following it; ELSE -> return the original string
+//examples
+//pseudocode
+//methdods
 
-console.log(removeAnchor("www.codewars.com#about"));
+const removeAnchor = url => (url.indexOf("#") === -1 ? url : url.split("#")[0]);
+
+console.log(removeAnchor("www.codewars.com#about"), "www.codewars.com");
+console.log(removeAnchor("www.codewars.com#contactus"), "www.codewars.com");
+console.log(removeAnchor("www.codewars.com?page=1"), "www.codewars.com?page=1");
