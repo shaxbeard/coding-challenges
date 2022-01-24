c85e30b909d654bcf95d59cc0a9f1a25982ef081
Eric Carlson <eric.carlson2972@gmail.com>
2022-01-24 07:09:32 -0500

    modified structure

--- invert-values-8arr.js
+++ invert-values-8arr.js
@@ -5,3 +5,10 @@
 // invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
 // invert([]) == []
 // You can assume that all values are integers. Do not mutate the input array/list.
+
+function invert(array) {
+  return array.map(n => n * -1);
+}
+
+console.log(invert([1, 2, 3, 4, 5]), [-1, -2, -3, -4, -5]);
+console.log(invert([1, -2, 3, -4, 5]), [-1, 2, -3, 4, -5]);
