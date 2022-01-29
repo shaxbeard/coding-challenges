82b6dda5b635d07f8fa16b245e9d6e05f395de39
Eric Carlson <eric.carlson2972@gmail.com>
2022-01-29 06:28:42 -0500

    initial commit

---
+++ reverse-strings-ultimate.js
@@ -0,0 +1,11 @@
+/* CHALLENGE
+Given a string of text, write an algorithm that returns the text received in a reversed format. 
+E.g reverseString('algorithms') // should return 'smhtirogla'
+*/
+
+function reverseString(text) {
+  return text.split("").reverse().join("");
+}
+
+console.log(reverseString("algorithm"), "mhtirogla");
+console.log(reverseString("123456789"), "987654321");
