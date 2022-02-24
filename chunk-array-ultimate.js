03aed8cc11b9c2577d3300c26f8e971346bc0ce4
Eric Carlson <eric.carlson2972@gmail.com>
2022-02-24 08:18:54 -0500

    add the splice solution

--- chunk-array-ultimate.js
+++ chunk-array-ultimate.js
@@ -2,24 +2,31 @@
 // divide the array into many sub-arrays
 // where each sub-array has the specified length.
 
-function chunkArray(arr, size) {
-  let result = [];
+// LOOPING THROUGH THE ARRAY
+// function chunkArray(arr, size) {
+//   let result = [];
 
-  for (value of arr) {
-    let lastArray = result[result.length - 1];
-    if (!lastArray || lastArray.length == size) {
-      result.push([value]);
-    } else {
-      lastArray.push(value);
-    }
+//   for (value of arr) {
+//     let lastArray = result[result.length - 1];
+//     if (!lastArray || lastArray.length == size) {
+//       result.push([value]);
+//     } else {
+//       lastArray.push(value);
+//     }
+//     console.log(lastArray);
+//   }
+//   return result;
+// }
+
+// LOOPING THROUGH THE NUMBER OF CHUNKS
+
+function chunkArray(array, size) {
+  let result = [];
+  let arrayCopy = [...array];
+
+  while (arrayCopy.length > 0) {
+    result.push(arrayCopy.slice(0, size));
   }
   return result;
 }
-
-console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2), [
-  [1, 2],
-  [3, 4],
-  [5, 6],
-  [7, 8],
-  [9, 10],
-]);
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5));
