b0ff238e8f44c56f03d68ed1ba12ca0b3f170b79
Eric Carlson <eric.carlson2972@gmail.com>
2022-02-26 07:25:37 -0500

    easiest solution implemented

--- palindrome-checker-ultimate.js
+++ palindrome-checker-ultimate.js
@@ -1,1 +1,11 @@
 // Three approaches the solving the palindrome check
+
+// THE EASIEST WAY
+
+function palindromeChecker(str) {
+  const reversedStr = str.toLowerCase().split("").reverse().join("");
+  return str === reversedStr;
+}
+
+console.log(palindromeChecker("racecar"), true);
+console.log(palindromeChecker("algorithm"), false);
