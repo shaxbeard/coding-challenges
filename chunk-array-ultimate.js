5ddd6b1da970293fce57acc6e30100cd109e13d0
Eric Carlson <eric.carlson2972@gmail.com>
2022-02-27 08:19:51 -0500

    Add the slice solution

--- chunk-array-ultimate.js
+++ chunk-array-ultimate.js
@@ -30,3 +30,15 @@ function chunkArray(array, size) {
   return result;
 }
 console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5));
+
+//USING SLICE()
+
+function chunkArray(array, size) {
+  let result = [];
+
+  for (i = 0; i < array.length; i += size) {
+    let chunk = array.slice(i, i + size);
+    result.push(chunk);
+  }
+  return result;
+}
