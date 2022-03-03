4315025fa78f73a401f03cd54b7e4a005587cad1
Eric Carlson <eric.carlson2972@gmail.com>
2022-03-03 07:06:45 -0500

    final touches

--- palindrome-checker-ultimate.js
+++ palindrome-checker-ultimate.js
@@ -1,18 +1,21 @@
+// Palindrom Checker. Given a string of text, return true or false
+// indicating whether or not the text is a palindrome
+
 // Three approaches the solving the palindrome check
 
 // THE EASIEST WAY
 
-// function palindromeChecker(str) {
-//   const reversedStr = str.toLowerCase().split("").reverse().join("");
-//   return str === reversedStr;
-// }
+function palindromeChecker(str) {
+  const reversedStr = str.toLowerCase().split("").reverse().join("");
+  return str === reversedStr;
+}
 
 // LOOPING THROUGH AND COMPARING CHARACTERS
 
-// function palindromeChecker(str) {
-//   let charArray = str.toLowerCase().split("");
-//   return charArray.every((l, i) => l === charArray[charArray.length - i - 1]);
-// }
+function palindromeChecker(str) {
+  let charArray = str.toLowerCase().split("");
+  return charArray.every((l, i) => l === charArray[charArray.length - i - 1]);
+}
 
 // LOOPING THROUGH AND COMPARING CHARACTERS(OPTIMIZED)
 function palindromeChecker(str) {
