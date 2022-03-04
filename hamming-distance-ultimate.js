b13b918faa1bf94463646d6c9c64eb8f5dcf4e4a
Eric Carlson <eric.carlson2972@gmail.com>
2022-03-04 07:08:08 -0500

    initial commit

---
+++ hamming-distance-ultimate.js
@@ -0,0 +1,10 @@
+/* CHALLENGE
+Given two strings of equal length, calculate and return the the hamming distance.
+E.g hammingDistance('rover', 'river') // should return 1
+*/
+
+function hammingDistance(stringA, stringB) {
+  // code here
+}
+
+console.log(hammingDistance("rover", "river"), 1);
