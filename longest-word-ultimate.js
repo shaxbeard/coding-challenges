da00341876a71618c3aeaadf0d38c5bee20dcf11
Eric Carlson <eric.carlson2972@gmail.com>
2022-03-09 06:39:23 -0500

    initial commit

---
+++ longest-word-ultimate.js
@@ -0,0 +1,8 @@
+//Given a sentence, return the longest word in the string
+
+function longestWord(str) {
+  return str;
+}
+
+console.log(longestWord("Top Shelf Web Development Training on Scotch"));
+console.log(longestWord("BUILDING FOR THE NEXT BILLION USERS"));
