49fa63737b01b48a853702dfeea0419fa2ea9099
Eric Carlson <eric.carlson2972@gmail.com>
2022-03-11 06:49:11 -0500

    solution with for-loop

--- longest-word-ultimate.js
+++ longest-word-ultimate.js
@@ -1,7 +1,19 @@
 //Given a sentence, return the longest word in the string
 
+// USING A FOR LOOP
+
 function longestWord(str) {
-  return str;
+  let wordArray = str.split(" ");
+  let maxLength = 0;
+  let result = "";
+
+  for (let i = 0; i < wordArray.length; i++) {
+    if (wordArray[i].length > maxLength) {
+      maxLength = wordArray[i].length;
+      result = wordArray[i];
+    }
+  }
+  return result;
 }
 
 console.log(longestWord("Top Shelf Web Development Training on Scotch"));
