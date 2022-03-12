f75a2444dd41dc1ff2f093beb8bd042760197ef1
Eric Carlson <eric.carlson2972@gmail.com>
2022-03-12 07:09:44 -0500

    solution with reduce - verbose

--- longest-word-ultimate.js
+++ longest-word-ultimate.js
@@ -1,20 +1,36 @@
-//Given a sentence, return the longest word in the string
+// Given a sentence, return the longest word in the string
+
+// EXAMPLES
+// "Top Shelf Web Development Training on Scotch" -> "development"
+// "BUILDING FOR THE NEXT BILLION USERS" -> "BUILDING"
 
 // USING A FOR LOOP
 
-function longestWord(str) {
-  let wordArray = str.split(" ");
-  let maxLength = 0;
-  let result = "";
+// function longestWord(str) {
+//   let wordArray = str.split(" ");
+//   let maxLength = 0;
+//   let result = "";
 
-  for (let i = 0; i < wordArray.length; i++) {
-    if (wordArray[i].length > maxLength) {
-      maxLength = wordArray[i].length;
-      result = wordArray[i];
-    }
-  }
-  return result;
-}
+//   for (let i = 0; i < wordArray.length; i++) {
+//     if (wordArray[i].length > maxLength) {
+//       maxLength = wordArray[i].length;
+//       result = wordArray[i];
+//     }
+//   }
+//   return result;
+// }
+
+// // USING REDUCE()
+// function longestWord(str) {
+//   const result = str.split(" ").reduce((maxLengthWord, currentWord) => {
+//     if (currentWord.length > maxLengthWord.length) {
+//       return currentWord;
+//     } else {
+//       return maxLengthWord;
+//     }
+//   }, "");
+//   return result;
+// }
 
 console.log(longestWord("Top Shelf Web Development Training on Scotch"));
 console.log(longestWord("BUILDING FOR THE NEXT BILLION USERS"));
