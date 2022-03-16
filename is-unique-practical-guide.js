eeeaaf550a71637b924877b4928c0f5f11a5310c
Eric Carlson <eric.carlson2972@gmail.com>
2022-03-16 09:58:04 -0400

    add solution with cache

--- is-unique-practical-guide.js
+++ is-unique-practical-guide.js
@@ -1,14 +1,31 @@
+// NESTED FOR() LOOPS
+// function isUnique(arr) {
+//   let result = true;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(`~~~~~~~  OUTER LOOP  ~~~~~~~~~ i === ${i}`);
+
+//     for (let j = 0; j < arr.length; j++) {
+//       console.log(`~~~~~~~  INNER LOOP  ~~~~~~~~~ j === ${j}`);
+//       if (i !== j && arr[i] === arr[j]) {
+//         result = false;
+//       }
+//     }
+//   }
+//   return result;
+// }
+
+// ONE FOR() LOOP WITH A CACHE
 function isUnique(arr) {
+  const cache = {};
   let result = true;
 
   for (let i = 0; i < arr.length; i++) {
-    console.log(`~~~~~~~  OUTER LOOP  ~~~~~~~~~ i === ${i}`);
-
-    for (let j = 0; j < arr.length; j++) {
-      console.log(`~~~~~~~  INNER LOOP  ~~~~~~~~~ j === ${j}`);
-      if (i !== j && arr[i] === arr[j]) {
-        result = false;
-      }
+    console.log(`~~~~~~ LOOP ~~~~~~~ i = ${i}`);
+    if (cache[arr[i]]) {
+      result = false;
+    } else {
+      cache[arr[i]] = true;
     }
   }
   return result;
