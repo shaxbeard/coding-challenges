d7097d51be830eef8cd10eb179300bdd69ade1a2
Eric Carlson <eric.carlson2972@gmail.com>
2022-03-20 10:31:22 -0400

    Solution with Index and Slice

--- search-and-replace-ultimate.js
+++ search-and-replace-ultimate.js
@@ -1,15 +1,16 @@
-function searchReplace(str, word, newWord) {
-  if (word[0] === word[0].toUpperCase()) {
-    newWord = newWord[0].toUpperCase() + newWord.slice(1);
-  }
-  return str.replace(word, newWord);
+// function searchReplace(str, word, newWord) {
+//   if (word[0] === word[0].toUpperCase()) {
+//     newWord = newWord[0].toUpperCase() + newWord.slice(1);
+//   }
+//   return str.replace(word, newWord);
+// }
+
+function searchReplace(str, w, newW) {
+  w[0] === w[0].toUpperCase() && newW[0].toUpperCase() + newW.slice(1);
+  return str.replace(w, newW);
 }
 
 console.log(
-  searchReplace(
-    "He is Sleeping on the couch",
-    "Sleeping",
-    "sitting",
-    "He is Sitting on the couch"
-  )
+  searchReplace("He is Sleeping on the couch", "Sleeping", "sitting"),
+  "He is Sitting on the couch"
 );
