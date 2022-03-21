edbccf80c1c58720adb771dc6300fde5fce693d4
Eric Carlson <eric.carlson2972@gmail.com>
2022-03-21 07:25:18 -0400

    adding more solutions

--- count-characters-in-your-string-6str.js
+++ count-characters-in-your-string-6str.js
@@ -4,18 +4,15 @@
 //Return - object with key=char, value=count for char
 //methods
 
-// function count(string) {
-//   let obj = {};
-//   //loop over string
-//   //if new char, create new key
-//   //increment the counter for that key
-//   string.split("").forEach((letter) => {
-//     if (obj[letter] === undefined) {
-//       obj[letter] === 0;
-//     }
-//     obj[letter]++;
-//   });
-// }
+function count(string) {
+  let obj = {};
+  string.split("").forEach(letter => {
+    if (obj[letter] === undefined) {
+      obj[letter] === 0;
+    }
+    obj[letter]++;
+  });
+}
 
 // FOR EACH METHOD
 // function count(string) {
@@ -26,13 +23,13 @@
 //   return obj;
 // }
 
-//BEST PRACTICE
-function count(string) {
-  return string.split("").reduce((obj, letter) => {
-    !obj[letter] ? (obj[letter] = 1) : obj[letter]++;
-    return obj;
-  }, {});
-}
+//BEST PRACTICE WITH REDUCE()
+// function count(string) {
+//   return string.split("").reduce((obj, letter) => {
+//     !obj[letter] ? (obj[letter] = 1) : obj[letter]++;
+//     return obj;
+//   }, {});
+// }
 
 console.log(count("aba"));
 console.log(count("abracadabra"));
