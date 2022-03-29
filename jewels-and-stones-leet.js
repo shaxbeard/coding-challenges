1dd6545c445a1b855dd290d626bf4a19c27f8166
Eric Carlson <eric.carlson2972@gmail.com>
2022-03-29 07:29:25 -0400

    hash map added with reduce

--- jewels-and-stones-leet.js
+++ jewels-and-stones-leet.js
@@ -7,10 +7,18 @@
 
 // Input - two string - str1 is a set of unique chars; str2 is chars that may or may not match the unique chars in str1
 // return - the total number of matches in str2 from the chars in str1
+// hash map, if char in jewels, then set property to true
 // counter, loop, if letter in "jewels" is in "stones", counter++
 
 function numJewelsInStones(jewels, stones) {
-  return jewels;
+  //hash map
+  const map = jewels.split("").reduce((obj, l) => {
+    !obj[l] && (obj[l] = true);
+    return obj;
+  }, {});
+  console.log(map);
+
+  //loop
 }
 
 console.log(numJewelsInStones("aA", "aAAbbbb"), 3);
