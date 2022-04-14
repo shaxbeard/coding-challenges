0fdfc4203a7ee7e2ca0852f74eca6597edbb5b34
Eric Carlson <eric.carlson2972@gmail.com>
2022-04-14 07:54:56 -0400

    initial commit

---
+++ is-anagram-ultimate.js
@@ -0,0 +1,8 @@
+// DIRECT COMPARISON
+
+function isAnagram(strA, strB) {
+  return strA;
+}
+
+console.log(isAnagram("silent", "listen"), true);
+console.log(isAnagram("skywalker", "listen"), false);
