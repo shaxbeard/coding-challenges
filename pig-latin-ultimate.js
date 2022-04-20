e3941c86dc26c1f782791369bfa45be40e58f9c2
Eric Carlson <eric.carlson2972@gmail.com>
2022-04-20 17:39:34 -0400

    initial commit

---
+++ pig-latin-ultimate.js
@@ -0,0 +1,18 @@
+// Translate the provided string to Pig Latin by following the rules below:
+
+// For words that begin with consonant sounds,
+// the consonant before the initial vowel should be moved to the end of the word sequence
+// and "ay" affixed. E.g "pig" = "igpay"
+// For words that begin with consonant clusters,
+// the clusters should be moved to the end of the word sequence and "ay" affixed. E.g
+// "glove" = "oveglay"
+// For words that begin with vowel sounds, simply add "way" to the end of the word. E.g
+// "explain" = "explainway”
+
+function pigLatin(str) {
+  return str;
+}
+
+console.log(pigLatin("pig"), "igpay");
+console.log(pigLatin("glove"), "oveglay");
+console.log(pigLatin("explain"), "explainway");
