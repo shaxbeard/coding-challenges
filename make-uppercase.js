6307b3a0070c43af7d70b2b9c2c935c4283d8362
Eric Carlson <eric.carlson2972@gmail.com>
2022-04-21 08:17:47 -0400

    initial commit

---
+++ make-uppercase.js
@@ -0,0 +1,9 @@
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+console.log(makeUpperCase("hello"), "HELLO");
+console.log(makeUpperCase(""), "");
+console.log(makeUpperCase("HELLO"), "HELLO");
