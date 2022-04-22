f4897c9f84ffc108dbcb899139e01998c77946fd
Eric Carlson <eric.carlson2972@gmail.com>
2022-04-22 17:51:26 -0400

    imperative solution with includes and slice

--- pig-latin-ultimate.js
+++ pig-latin-ultimate.js
@@ -10,7 +10,21 @@
 // "explain" = "explainway”
 
 function pigLatin(str) {
-  return str;
+  str = str.toLowerCase();
+  const vowels = ["a", "e", "i", "o", "u"];
+  let vowelIndex = 0;
+
+  if (vowels.includes(str[0])) {
+    return str + "way";
+  } else {
+    for (let char of str) {
+      if (vowels.includes(char)) {
+        vowelIndex = str.indexOf(char);
+        break;
+      }
+    }
+  }
+  return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
 }
 
 console.log(pigLatin("pig"), "igpay");
