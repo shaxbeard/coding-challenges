9eca18c71544ae38565df1f0515f1a53a7b459e2
Eric Carlson <eric.carlson2972@gmail.com>
2022-04-28 08:08:01 -0400

    initial commit

---
+++ chunk-array-ultimate.js
@@ -0,0 +1,15 @@
+// Given an array and a chunk size,
+// divide the array into many sub-arrays
+// where each sub-array has the specified length.
+
+function chunkArray(arr, size) {
+  return arr;
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2), [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+  [7, 8],
+  [9, 10],
+]);
