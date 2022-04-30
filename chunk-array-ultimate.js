d3bf540281d09178531a778cb6f05eef8f2d29ee
Eric Carlson <eric.carlson2972@gmail.com>
2022-04-30 08:13:53 -0400

    solution looping through the entire array with for-of

--- chunk-array-ultimate.js
+++ chunk-array-ultimate.js
@@ -3,7 +3,17 @@
 // where each sub-array has the specified length.
 
 function chunkArray(arr, size) {
-  return arr;
+  let result = [];
+
+  for (value of arr) {
+    let lastArray = result[result.length - 1];
+    if (!lastArray || lastArray.length == size) {
+      result.push([value]);
+    } else {
+      lastArray.push(value);
+    }
+  }
+  return result;
 }
 
 console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2), [
