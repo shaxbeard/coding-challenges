be02cdd7f1a0786e2a5c377f907c3b53188782ce
Eric Carlson <eric.carlson2972@gmail.com>
2022-05-06 06:16:52 -0400

    add prep

--- count-positives-sum-negatives.js
+++ count-positives-sum-negatives.js
@@ -9,6 +9,13 @@
 // Example
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
+//PREP
+//Parameter -
+//Return -
+//Example -
+//Pseudocode -
+//Methods!
+
 function countPositivesSumNegatives(input) {
   if (!input || !input.length) return [];
   let count = 0;
