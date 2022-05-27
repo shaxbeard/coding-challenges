19accc18a2e0b5fe1907bb3686fc3396651e3dd7
Eric Carlson <eric.carlson2972@gmail.com>
2022-05-26 23:28:11 -0400

    delete earlier version of the even numbers algo

--- even-numbers-in-an-array.js
+++
@@ -1,7 +0,0 @@
-const arr = [-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26];
-
-function evenNumbers(array, number) {
-  return array.filter((x) => x % 2 === 0).slice(-number);
-}
-
-console.log(evenNumbers(arr, 2));
