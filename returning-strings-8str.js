dd996693d497f47e6a146d81125c7c849a7504fc
Eric Carlson <eric.carlson2972@gmail.com>
2022-06-03 06:36:49 -0400

    initial commit

---
+++ returning-strings-8str.js
@@ -0,0 +1,12 @@
+//PREP
+//I accept a name and return "Hello, <name> how are you doing today?"
+//P: String only? Could it be Elon musk child with numbers? If numbers are strings
+//R: I return the value as the return value of a named function
+//E: "Eric"  // "Hello, Eric how are you doing today?"
+
+function greet(name) {
+  //Insert variable into another string
+  return `Hello, ${name} how you are doing today?";`;
+}
+
+console.log(greet("Eric"));
