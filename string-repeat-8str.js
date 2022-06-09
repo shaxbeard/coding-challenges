b465c27cea22a763bbbba06ff30f90a67daefcab
Eric Carlson <eric.carlson2972@gmail.com>
2022-06-09 06:10:09 -0400

    initial commit

---
+++ string-repeat-8str.js
@@ -0,0 +1,19 @@
+// Write a function that accepts an integer n and a string s as parameters
+// and returns a string of s repeated exactly n times.
+
+const str = "Hello";
+
+// function repeatStr(n, s) {
+//   let str = "";
+//   for (let i = 0; i < n; i++) {
+//     str += s;
+//   }
+//   return str;
+// }
+// console.log(repeatStr(17, str));
+
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
+
+console.log(repeatStr(99, str));
