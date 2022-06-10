8687fa306bfcdbc324efa889cbec940cf9311f52
Eric Carlson <eric.carlson2972@gmail.com>
2022-06-10 06:09:23 -0400

    initial commit

---
+++ string-ends-with-str-7str.js
@@ -0,0 +1,20 @@
+//PREP
+//If str ends with ending TRUE, else FALSE
+//Parameters
+//Return
+//Examples
+//Pseudocode
+
+//Desired result
+//solution('abc', 'bc') -> true
+//solution('abc', 'd') -> false
+
+//solution('abcdefgh', 'fgh') -> true
+
+//LOL - I guess I should look at the list of JS string methods first
+
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
+
+console.log(solution("abc", "bc"));
