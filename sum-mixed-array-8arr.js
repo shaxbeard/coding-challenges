8ab39176bc089155ee99689728678709e1b27413
Eric Carlson <eric.carlson2972@gmail.com>
2022-06-12 06:12:00 -0400

    initial commit

---
+++ sum-mixed-array-8arr.js
@@ -0,0 +1,10 @@
+// Given an array of integers as strings and numbers,
+//return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+function sumMix(x) {
+  return x.map(Number).reduce((total, item) => total + item, 0);
+}
+
+console.log(sumMix([1, 2, "3", "4"]));
