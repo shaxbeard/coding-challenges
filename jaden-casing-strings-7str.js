55969fff1ce8b6f3d0245fb33b5aefc402b1bc5c
Eric Carlson <eric.carlson2972@gmail.com>
2022-07-29 06:51:21 -0400

    initial commit

---
+++ jaden-casing-strings-7str.js
@@ -0,0 +1,16 @@
+//A string with multiple words is inputted - each word should be changed so it begin with capital letter
+//Inputs: a string, letters only not numbers or special characters
+//return the full string with intial caps on all letters
+
+function jadenCase(str) {
+  return str
+    .split(" ")
+    .map((item) => item[0].toUpperCase() + item.slice(1))
+    .join(" ");
+}
+
+//Desired Result
+// jadenCase("How can mirrors be real if our eyes aren't real"); // "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+console.log(jadenCase("hello world"));
+console.log(jadenCase("How can mirrors be real if our eyes aren't real"));
