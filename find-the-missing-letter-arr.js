11413530cb7c3e2fc858b9e7da66c2c75f7adcda
Eric Carlson <eric.carlson2972@gmail.com>
2022-09-05 06:15:21 -0400

    initial commit

---
+++ find-the-missing-letter-arr.js
@@ -0,0 +1,17 @@
+let arr = ["a", "b", "c", "d", "f"];
+
+const findMissingLetters = (array) => {
+  let missingLetter;
+  arr.forEach((item, i, { [i - 1]: prev }) => {
+    if (i > 0) {
+      missingLetter =
+        item.charCodeAt() !== prev.charCodeAt() + 1
+          ? String.fromCharCode(item.charCodeAt() - 1)
+          : null;
+    }
+  });
+  return missingLetter;
+};
+
+let test = findMissingLetters(arr);
+console.log(test);
