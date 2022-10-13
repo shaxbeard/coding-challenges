b30de1c454db2a81790dd2aa785eff47964b336d
Eric Carlson <eric.carlson2972@gmail.com>
2022-10-13 06:22:45 -0400

    add the prep method

--- returning-strings-8str.js
+++ returning-strings-8str.js
@@ -3,6 +3,7 @@
 //P: String only? Could it be Elon musk child with numbers? If numbers are strings
 //R: I return the value as the return value of a named function
 //E: "Eric"  // "Hello, Eric how are you doing today?"
+//Methods
 
 function greet(name) {
   //Insert variable into another string
