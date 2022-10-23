b202fb7d52a535cb580b3e4d8d93eb752aa29bcf
Eric Carlson <eric.carlson2972@gmail.com>
2022-10-23 06:55:11 -0400

    initial commit

---
+++ remove-anchor-from-url-7str.js
@@ -0,0 +1,10 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeAnchor(url) {
+  return url;
+}
+
+console.log(removeAnchor("www.codewars.com#about"));
