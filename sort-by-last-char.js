700f3aebe72778aec9be0d80b75dc7ddc82d6d77
Eric Carlson <eric.carlson2972@gmail.com>
2022-11-03 06:40:17 -0400

    initial comit

---
+++ sort-by-last-char.js
@@ -0,0 +1,31 @@
+// Given a string of words (x), you need to return an array of the words,
+// sorted alphabetically by the final character in each.
+
+// If two words have the same last letter,
+// they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+// Parameters
+// return
+// Examples
+// Psuedocode
+// Methods
+
+function last(str) {
+  return str;
+}
+
+console.log(last("man i need a taxi up to ubud"), [
+  "a",
+  "need",
+  "ubud",
+  "i",
+  "taxi",
+  "man",
+  "to",
+  "up",
+]);
+
+// (last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']);
+// (last('take me to semynak'), ['take', 'me', 'semynak', 'to']);
