ccfb62eb9431786d7ab918485ee31b96c3ba7162
Eric Carlson <eric.carlson2972@gmail.com>
2022-11-14 21:38:49 -0500

    initial commit

--- find-the-missing-letter-arr.js
+++ find-the-missing-letter-arr.js
@@ -5,19 +5,19 @@
 //Pseudocode -
 //Methods!
 
-let arr = ["a", "b", "c", "d", "f"];
-
-const findMissingLetters = (array) => {
+const findMissingLetters = myArr => {
   let missingLetter;
-  arr.forEach((item, i, { [i - 1]: prev }) => {
+  myArr.forEach((item, i, { [i - 1]: prev, [i + 1]: next }) => {
+    // what is { [i - 1: prev]}? AHA! It is the ITEM at index-1 set to the variable of "prev"
     if (i > 0) {
       missingLetter =
-        item.charCodeAt() !== prev.charCodeAt() + 1 &&
-        String.fromCharCode(item.charCodeAt() - 1);
+        item.charCodeAt() !== prev.charCodeAt() + 1 && //if the current item's charCode != the prev item's charCode + 1
+        String.fromCharCode(item.charCodeAt() - 1); // then the charCode of the missing letter = the current item's charCode - 1
     }
   });
   return missingLetter;
 };
 
-let test = findMissingLetters(arr);
-console.log(test);
+//Examples
+console.log(findMissingLetters(["a", "b", "c", "d", "f"]), "e");
+console.log(findMissingLetters(["w", "x", "z"]), "y");
