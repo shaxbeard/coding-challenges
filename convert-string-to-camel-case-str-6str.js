67d84ce77d26ce1480fbf347f6a47d6d0faa0cb2
Eric Carlson <eric.carlson2972@gmail.com>
2022-11-15 06:28:40 -0500

    updated solution

--- convert-string-to-camel-case-str-6str.js
+++ convert-string-to-camel-case-str-6str.js
@@ -15,32 +15,12 @@
 
 function toCamelCase(str) {
   const strSplit = str.indexOf("-") !== -1 ? str.split("-") : str.split("_");
-  const initialCaps = strSplit.map((item, index) => {
-    return index === 0 ? item : item[0].toUpperCase() + item.slice(1);
-  });
-  return initialCaps.join("");
-
-  // const strSplit = str.indexOf("-") !== -1 ? str.split("-") : str.split("_");
-  //   let camel;
-  //   strSplit.forEach((item, index) => {
-  //     index === 0
-  //       ? (camel = item) // Add the first word unchanged
-  //       : (camel += item[0].toUpperCase() + item.slice(1)); // for the other words, add the first letter as uppercase, then add the rest of the word
-  //   });
-  //   return camel;
-  // }
-
-  // const strSplit = str.indexOf("-") !== -1 ? str.split("-") : str.split("_");
-
-  // const camel = strSplit.reduce((total, item, index) => {
-  //   return (total =
-  //     index === 0
-  //       ? (total = item)
-  //       : total + item[0].toUpperCase() + item.slice(1));
-  // }, "");
-  // return camel;
+  return strSplit
+    .map((word, i) => (i === 0 ? word : word[0].toUpperCase() + word.slice(1)))
+    .join("");
 }
 
 console.log(toCamelCase("the-stealth-warrier"), "theStealthWarrior");
 console.log(toCamelCase("the_stealth_warrier"), "theStealthWarrior");
 console.log(toCamelCase("The_Stealth_Warrier"), "TheStealthWarrior");
+// ["the"], ["stealth"], ["warrior]"];
