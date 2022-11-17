1265884b9faa985db7e704e8ffd53b4cf2e1d443
Eric Carlson <eric.carlson2972@gmail.com>
2022-11-17 06:31:09 -0500

    solved with repeat method

---
+++ average-length-7arr.js
@@ -0,0 +1,29 @@
+// Given an array of strings of the same letter type.
+// Return a new array, which will differ
+// in that the length of each element
+// is equal to the average length of the elements of the previous array.
+// If the average length is not an integer, use Math.round().
+
+//PREP
+// input is an array of strings with the same letter
+// return? an array with the same elements
+// BUT - the length of each element will be changed to the avg length of all elelments in orignal array
+
+// A few examples:
+
+// ['u', 'y'] =>  ['u', 'y'] // average length is 1
+// ['aa', 'bbb', 'cccc'] => ['aaa', 'bbb', 'ccc'] // average length is 3
+// ['aa', 'bb', 'ddd', 'eee'] => ['aaa', 'bbb', 'ddd', 'eee'] // average length is 2.5 round up to 3
+
+function averageLength(arr) {
+  const avg = Math.round(arr.reduce((a, c) => a + c.length, 0) / arr.length);
+  return arr.map(item => item[0].repeat(avg));
+}
+
+console.log(averageLength(["aa", "bbb", "cccc"]), ["aaa", "bbb", "ccc"]);
+console.log(averageLength(["aa", "bb", "ddd", "eee"]), [
+  "aaa",
+  "bbb",
+  "ddd",
+  "eee",
+]);
