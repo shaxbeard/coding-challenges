fe034d8fc6108104e00fd43e7f4b8e985e22f46e
Eric Carlson <eric.carlson2972@gmail.com>
2022-11-21 21:21:58 -0500

    initial commit

---
+++ remove-consecutive-duplicate-words-7arr.js
@@ -0,0 +1,17 @@
+// Your task is to remove all consecutive duplicate words from a string,
+// leaving only first words entries.
+
+// Examples
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta"
+
+function removeConsecutiveDuplicates(str) {
+  return str;
+}
+
+console.log(
+  removeConsecutiveDuplicates(
+    "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+  )
+);
