70a27393d50f9e036441abace53183a948d4ac44
Eric Carlson <eric.carlson2972@gmail.com>
2022-12-05 07:13:06 -0500

    initial commit

---
+++ search-and-replace-ultimate.js
@@ -0,0 +1,12 @@
+function searchReplace(str, word, newWord) {
+  return str;
+}
+
+console.log(
+  searchReplace(
+    "He is Sleeping on the couch",
+    "Sleeping",
+    "sitting",
+    "He is Sitting on the couch"
+  )
+);
