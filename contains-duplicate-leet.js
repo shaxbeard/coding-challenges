d6302363b8267de8d0cf5b7d23cce73ff95c9b25
Eric Carlson <eric.carlson2972@gmail.com>
2022-12-09 06:22:15 -0500

    soltuion with nested for loops

--- contains-duplicate-leet.js
+++ contains-duplicate-leet.js
@@ -19,9 +19,69 @@
 // Output: true
 
 // USING SET
+// const containsDuplicate = function (nums) {
+//   const uniques = [...new Set([...nums])];
+//   return uniques.length !== nums.length;
+// };
+
+// USING NESTED FOR LOOPS - IS THIS CORRECT ??
 const containsDuplicate = function (nums) {
-  const uniques = [...new Set([...nums])];
-  return uniques.length !== nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    for (let k = i + 1; k < nums.length; k++) {
+      if (nums[i] === nums[k]) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+// USING A MAP
+// const containsDuplicate = function (nums) {
+//   // first you create a hash map
+//   const map = nums.reduce((obj, el) => {
+//     !obj[el] ? (obj[el] = 1) : obj[el]++;
+//     return obj;
+//   }, {});
+
+//   //then you loop over the map and return true IF the map contains a duplicate
+//   for (num in map) {
+//     if (map[num] > 1) {
+//       return true;
+//     }
+//   }
+//   return false;
+// };
+
+// USING A FOR LOOP
+// function containsDuplicate(arr) {
+//   let numsMap = {};
+//   for (let i = 0; i < arr.length; i++) {
+//     let num = arr[i];
+//     // as you are building the map, return true as soon as you find a duplicate
+//     if (numsMap[num]) {
+//       return true;
+//       // if no duplicate, then just set the first instance of a num to true
+//     } else {
+//       numsMap[num] = true;
+//     }
+//   }
+//   return false;
+// }
+
+// USING FOR-OF
+const containsDuplicate = function (arr) {
+  const numsMap = {};
+  for (num of arr) {
+    // as you are building the map, return true as soon as you find a duplicate
+    if (numsMap[num]) {
+      return true;
+      // if no duplicate, then just set the first instance of a num to true
+    } else {
+      numsMap[num] = true;
+    }
+  }
+  return false;
 };
 
 console.log(containsDuplicate([1, 2, 3, 1]), true);
