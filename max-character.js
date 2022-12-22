a3d3834c7e7d716a7ea03b5ff5e51cff9e2a610e
Eric Carlson <eric.carlson2972@gmail.com>
2022-12-22 07:19:51 -0500

    Add comments

--- max-character.js
+++ max-character.js
@@ -8,34 +8,35 @@
 
 // No String or Array Methods (well brute force it first, but then no methods)!
 
-function maxCharacter(str) {
-  // char map
-  const map = {};
-  for (const char of str) {
-    !map[char] ? (map[char] = 1) : map[char]++;
-  }
-  // find the highest number in the char map
-  let maxCount = 0;
-  let maxChar = "";
-  for (const char in map) {
-    if (map[char] > maxCount) {
-      maxCount = map[char];
-      maxChar = char;
-    }
-  }
-  return maxChar;
-}
-
 // function maxCharacter(str) {
-//   function charMap(str) {
-//     const map = {};
-//     for (const char of str) {
-//       !map[char] ? (map[char] = 1) : map[char]++;
-//     }
-//     return map;
+//   // char map
+//   const map = {};
+//   for (const char of str) {
+//     !map[char] ? (map[char] = 1) : map[char]++;
 //   }
-//   const map = charMap(str);
-// return Object.entries(map).sort(([, a], [, b]) => b - a)[0][0];
+//   // find the highest number in the char map
+//   let maxCount = 0;
+//   let maxChar = "";
+//   for (const char in map) {
+//     if (map[char] > maxCount) {
+//       maxCount = map[char];
+//       maxChar = char;
+//     }
+//   }
+//   return maxChar;
 // }
+
+function maxCharacter(str) {
+  function charMap(str) {
+    const map = {};
+    for (const char of str) {
+      !map[char] ? (map[char] = 1) : map[char]++;
+    }
+    return map;
+  }
+  const map = charMap(str);
+  console.log(Object.values(map));
+  // return Object.entries(map).sort(([, a], [, b]) => b - a)[0][0];
+}
 console.log(maxCharacter("Hello World!"), "l");
-console.log(maxCharacter("abracadabra"), "a");
+// console.log(maxCharacter("abracadabra"), "a");
