166136d222faf4459f48ceb6034c313ebf225e0d
Eric Carlson <eric.carlson2972@gmail.com>
2022-12-21 22:09:04 -0500

    for loop solution

--- fibonacci-leet.js
+++ fibonacci-leet.js
@@ -22,8 +22,16 @@
 // Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 // The full sequence with 4 numbers: [0,1,1,2]
 
+//  FOR() LOOP SOLUTION
 function fib(n) {
-  return n;
+  let arr = [0, 1]; // because we ALWAYS have 0 and 1?
+  for (let i = 2; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+    //iteration #1 - (i=2) arr[2] = arr[1] (=1) + arr[0] (=0) - so arr = [0,1,1]
+    //iteration #2 - (i=3) arr[3] = arr[2] (=1) + arr[1] (=1) - so arr = [0,1,1,2]
+    //iteration #3 - (i=4) arr[4] = arr[3] (=2) + arr[2] (=1) = so arr = [0,1,1,2,3]
+  }
+  return arr[n]; //arr[4] = [0,1,1,2,3] (index 4 = 3)
 }
 
 console.log(fib(4), 3);
