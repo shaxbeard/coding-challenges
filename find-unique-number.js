0822420f76a96e687a7d9f7424173ae6eeccfbf1
Eric Carlson <eric.carlson2972@gmail.com>
2022-12-26 07:20:06 -0500

    brute force solution with nested loops

--- find-unique-number.js
+++ find-unique-number.js
@@ -6,9 +6,25 @@
 
 // The tests contain some very huge arrays, so think about performance.
 
+// Input is an array with numbers - integers, decimals, no numbers as string
+// Output is the single unique number
+// Methods - set, indexOf?
+// Brute force - nested loop
+
 function findUniq(arr) {
-  return arr;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] !== arr[j]) {
+        if (j === 1) {
+          return arr[i];
+        }
+        return arr[j];
+      }
+    }
+  }
 }
 
 console.log(findUniq([1, 1, 1, 2, 1, 1]), 2);
-console.log(findUniq([0, 0, 0.55, 0, 0]), 0.55);
+// console.log(findUniq([0, 0, 0.55, 0, 0]), 0.55);
+// console.log(findUniq([2, 1, 1, 1, 1, 1]), 2);
+// console.log(findUniq([1, 1, 1, 1, 1, 2]), 2);
