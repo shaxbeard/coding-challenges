9ba2f1e84291d4f04f489895b52dde728cdc01aa
Eric Carlson <eric.carlson2972@gmail.com>
2022-12-31 07:58:07 -0500

    Fastest solution with for loop and clear

--- find-unique-number.js
+++ find-unique-number.js
@@ -11,20 +11,46 @@
 // Methods - set, indexOf?
 // Brute force - nested loop
 
-function findUniq(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[i] !== arr[j]) {
-        if (j === 1) {
-          return arr[i];
-        }
-        return arr[j];
-      }
-    }
-  }
-}
+// FOR() SOLUTION -- FASTEST PERFORMANCE
+// function findUniq(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== arr[arr.length - 1]) {
+//       if (arr[i] !== arr[i + 1]) {
+//         return arr[i];
+//       } else {
+//         return arr[arr.length - 1];
+//       }
+//     }
+//   }
+// }
 
-console.log(findUniq([1, 1, 1, 2, 1, 1]), 2);
-console.log(findUniq([0, 0, 0.55, 0, 0]), 0.55);
+// INDEX OF() - 92% SLOWER THAN FOR()
+// function findUniq(arr) {
+//   for (let n of arr) {
+//     if (arr.indexOf(n) === arr.lastIndexOf(n)) {
+//       return n;
+//     }
+//   }
+// }
+
+// THIS WAS MY ATTEMPT AT A FOR() LOOP SOLUTION - CANNOT HANDLE BOTH [1,0,0] AND [0,1,0] ??
+// The for() solution solves this issue by comparing the LAST element in the array not the second # Why does this work better?
+// function findUniq(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== arr[i + 1]) {
+//       if (i === 0) {
+//         return arr[i];
+//       } else {
+//         return arr[i + 1];
+//       }
+//     }
+//   }
+// }
+
+// console.log(findUniq([1, 0, 0]), 1);
+// console.log(findUniq([0, 1, 0]), 1);
+// console.log(findUniq([0, 0, 1]), 1);
+// console.log(findUniq([1, 1, 1, 2, 1, 1]), 2);
+// console.log(findUniq([0, 0, 0.55, 0, 0]), 0.55);
 // console.log(findUniq([2, 1, 1, 1, 1, 1]), 2);
 // console.log(findUniq([1, 1, 1, 1, 1, 2]), 2);
