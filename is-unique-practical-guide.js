d77c9b494a982b8dad60fc384ffc5cb25d4fc01b
Eric Carlson <eric.carlson2972@gmail.com>
2023-01-02 07:38:51 -0500

    cache solution and add performance info

--- is-unique-practical-guide.js
+++ is-unique-practical-guide.js
@@ -1,35 +1,34 @@
+// "Is Unique. Practical guide."
+// Given an array of numbers, return true if all of the numbers are unique, or false if there are duplicates.
+
 // NESTED FOR() LOOPS
-// function isUnique(arr) {
-//   let result = true;
-
-//   for (let i = 0; i < arr.length; i++) {
-//     console.log(`~~~~~~~  OUTER LOOP  ~~~~~~~~~ i === ${i}`);
-
-//     for (let j = 0; j < arr.length; j++) {
-//       console.log(`~~~~~~~  INNER LOOP  ~~~~~~~~~ j === ${j}`);
-//       if (i !== j && arr[i] === arr[j]) {
-//         result = false;
-//       }
-//     }
-//   }
-//   return result;
-// }
-
-// ONE FOR() LOOP WITH A CACHE
 function isUnique(arr) {
-  const cache = {};
   let result = true;
 
   for (let i = 0; i < arr.length; i++) {
-    console.log(`~~~~~~ LOOP ~~~~~~~ i = ${i}`);
-    if (cache[arr[i]]) {
-      result = false;
-    } else {
-      cache[arr[i]] = true;
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j && arr[i] === arr[j]) {
+        result = false;
+      }
     }
   }
   return result;
 }
+
+// ONE FOR() LOOP WITH A CACHE
+// function isUnique(arr) {
+//   const cache = {};
+//   let result = true;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (cache[arr[i]]) {
+//       result = false;
+//     } else {
+//       cache[arr[i]] = true;
+//     }
+//   }
+//   return result;
+// }
 
 console.log(isUnique([1, 2, 3]), true);
 console.log(isUnique([1, 1, 3]), false);
