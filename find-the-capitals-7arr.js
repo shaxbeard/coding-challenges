bfa2bf9a0ae5b6317a899499fe0c84b82f40d0cc
Eric Carlson <eric.carlson2972@gmail.com>
2023-03-05 14:13:08 -0500

    initial commit

---
+++ find-the-capitals-7arr.js
@@ -0,0 +1,8 @@
+// Write a function that takes a single string (word) as argument.
+// The function must return an ordered list containing the indexes of all capital letters in the string.
+
+var capitals = function (word) {
+  return word;
+};
+
+console.log(capitals("CodEWaRs"), [0, 3, 4, 6]);
