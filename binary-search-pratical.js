a5ae9480ffa0a5112dc0e0a111baa9c8b9020817
Eric Carlson <eric.carlson2972@gmail.com>
2023-03-17 07:39:08 -0400

    Add second version of the solution

--- binary-search-pratical.js
+++ binary-search-pratical.js
@@ -11,27 +11,41 @@
 
 // console.log(linearSearch([2, 6, 7, 90, 103], 90), 3);
 
-// BINARY SEARCH SOLUTION
-function binarySearch(arr, item) {
-  var min = 0;
-  var max = arr.length - 1;
-  var guess;
+// BINARY SEARCH SOLUTION #1
+// function binarySearch(arr, item) {
+//   var min = 0;
+//   var max = arr.length - 1;
+//   var guess;
 
-  while (min <= max) {
-    guess = Math.floor((min + max) / 2);
+//   while (min <= max) {
+//     guess = Math.floor((min + max) / 2);
 
-    if (arr[guess] === item) {
-      return guess;
+//     if (arr[guess] === item) {
+//       return guess;
+//     } else {
+//       if (arr[guess] < item) {
+//         min = guess + 1;
+//       } else {
+//         max = guess - 1;
+//       }
+//     }
+//   }
+//   return -1;
+// }
+
+// LEON'S SOLUTION
+function binarySearch(nums, target) {
+  let left = 0,
+    right = nums.length - 1;
+  while (left < right) {
+    let mid = left + Math.floor((right - left + 1) / 2);
+    if (target < nums[mid]) {
+      right = mid - 1;
     } else {
-      if (arr[guess] < item) {
-        min = guess + 1;
-      } else {
-        max = guess - 1;
-      }
+      left = mid;
     }
   }
-
-  return -1;
+  return nums[left] === target ? left : -1;
 }
 
 console.log(binarySearch([2, 6, 64, 90, 103, 200], 90), 3);
