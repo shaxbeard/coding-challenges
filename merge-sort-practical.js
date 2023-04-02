cc57cd1549c60e5159c2669c5b2c33af7cc06a55
Eric Carlson <eric.carlson2972@gmail.com>
2023-04-02 07:07:32 -0400

    new solution to merge sort

--- merge-sort-practical.js
+++ merge-sort-practical.js
@@ -1,8 +1,6 @@
-//TASK: implement mergesort!
-
 // Split the array into halves and merge them recursively
 function mergeSort(arr) {
-  if (arr.length === 1) {
+  if (arr.length <= 1) {
     // return once we hit an array with a single item
     return arr;
   }
