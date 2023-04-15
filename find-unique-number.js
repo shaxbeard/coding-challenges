39ce433daf9a657bcb3e7ab27283d8e965e388f8
Eric Carlson <eric.carlson2972@gmail.com>
2023-04-15 06:57:01 -0400

    add yet another solution

--- find-unique-number.js
+++ find-unique-number.js
@@ -11,21 +11,41 @@
 // Methods - set, indexOf?
 // Brute force - nested loop
 
-// FOR() SOLUTION -- FASTEST PERFORMANCE
+// NESTED LOOPS SOLUTION - 73% SLOWER
 // function findUniq(arr) {
 //   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] !== arr[arr.length - 1]) {
-//       //skip the iteration if the element !== the final element - how does this work?
-//       if (arr[i] !== arr[i + 1]) {
-//         return arr[i];
-//       } else {
-//         return arr[arr.length - 1];
+//     let isUnique = true;
+//     for (let j = 0; j < arr.length; j++) {
+//       if (i !== j && arr[i] === arr[j]) {
+//         // If the current number is not unique, set the flag and break out of the loop
+//         isUnique = false;
+//         break;
 //       }
 //     }
+//     if (isUnique) {
+//       return arr[i];
+//     }
 //   }
+//   return null;
 // }
 
-// INDEX OF() - 92% SLOWER THAN FOR()
+// FOR() SOLUTION -- FASTEST PERFORMANCE
+function findUniq(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[arr.length - 1]) {
+      if (arr[i] !== arr[i + 1]) {
+        return arr[i];
+      } else {
+        return arr[arr.length - 1];
+      }
+    }
+  }
+}
+
+// console.log(findUniq([1, 0, 0]), 1);
+// console.log(findUniq([0, 0, 1]), 1);
+
+// INDEX OF() - 96% SLOWER THAN FOR()
 // function findUniq(arr) {
 //   for (let n of arr) {
 //     if (arr.indexOf(n) === arr.lastIndexOf(n)) {
@@ -34,59 +54,33 @@
 //   }
 // }
 
-// THIS WAS MY ATTEMPT AT A FOR() LOOP SOLUTION - CANNOT HANDLE BOTH [1,0,0] AND [0,1,0] ??
-// The for() solution solves this issue by comparing the LAST element in the array not the second # Why does this work better?
+// USING A HASH MAP - 98% SLOWER THAN FOR()
 // function findUniq(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] !== arr[i + 1]) {
-//       if (i === 0) {
-//         return arr[i];
-//       } else {
-//         return arr[i + 1];
-//       }
+//   //Go through the entire array and store the frequency of each number in a hash map
+//   const map = {};
+//   for (const num of arr) {
+//     map[num] ? map[num]++ : (map[num] = 1);
+//   }
+
+//   // const map = arr.reduce((obj, num) => {
+//   //   obj[num] ? obj[num]++ : (obj[num] = 1);
+//   //   return obj;
+//   // }, {});
+
+//   // Loop through the map to find the number with the value of 1
+//   for (const num in map) {
+//     if (map[num] === 1) {
+//       return num;
 //     }
 //   }
+
+//   return map;
 // }
-
-// FOR() SOLUTION -- FASTEST PERFORMANCE
-// function findUniq(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     // compare each element with the final element in the array until you find a difference
-//     if (arr[i] !== arr[arr.length - 1]) {
-//       // IF arr[i] is also different from the NEXT element, then arr[i] is the unique one
-//       if (arr[i] !== arr[i + 1]) {
-//         return arr[i];
-//       } else {
-//         return arr[arr.length - 1]; // otherwise it is FINAL element that is unique
-//       }
-//     }
-//   }
-// }
-
-// THIS NESTED LOOP SEEMS TO WORK, BUT IT MUST BE QUADRATIC THUS SLOWER?
-// function findUniq(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = 0; j < arr.length; j++) {
-//       if (i !== j && arr[i] !== arr[j]) {
-//         return arr[i];
-//       }
-//     }
-//   }
-// }
-
-// USING A HASH MAP
-
-//Go through the entire array and store the frequency of each number in a hash map
-function findUniq(arr) {
-  return arr;
-}
-
-//Loop through the map to find the number with the value of 1
 
 // console.log(findUniq([1, 0, 0]), 1);
 // console.log(findUniq([0, 1, 0]), 1);
 // console.log(findUniq([0, 0, 1]), 1);
-console.log(findUniq([1, 1, 1, 2, 1, 1]), 2);
-console.log(findUniq([0, 0, 0.55, 0, 0]), 0.55);
-console.log(findUniq([2, 1, 1, 1, 1, 1]), 2);
-console.log(findUniq([1, 1, 1, 1, 1, 2]), 2);
+console.log(findUniq([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1]), 2);
+// console.log(findUniq([0, 0, 0.55, 0, 0]), 0.55);
+// console.log(findUniq([2, 1, 1, 1, 1, 1]), 2);
+// console.log(findUniq([1, 1, 1, 1, 1, 2]), 2);
