b7036c102e49db59f018600188f3a384a73a1947
Eric Carlson <eric.carlson2972@gmail.com>
2023-05-27 06:24:21 -0400

    Small change to solution

--- pig-latin-ultimate.js
+++ pig-latin-ultimate.js
@@ -10,31 +10,31 @@
 // "explain" = "explainway”
 
 // IMPERATIVE WAY
-function pigLatin(str) {
-  str = str.toLowerCase();
-  const vowels = ["a", "e", "i", "o", "u"];
-  let vowelIndex = 0;
+// function pigLatin(str) {
+//   str = str.toLowerCase();
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   let vowelIndex = 0;
 
-  if (vowels.includes(str[0])) {
-    return str + "way";
-  } else {
-    for (let char of str) {
-      if (vowels.includes(char)) {
-        vowelIndex = str.indexOf(char);
-        break;
-      }
-    }
-  }
-  return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
-}
+//   if (vowels.includes(str[0])) {
+//     return str + "way";
+//   } else {
+//     for (let char of str) {
+//       if (vowels.includes(char)) {
+//         vowelIndex = str.indexOf(char);
+//         break;
+//       }
+//     }
+//   }
+//   return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
+// }
 
 // DECLARATIVE WAY
 
-// function pigLatin(str) {
-//   return str
-//     .replace(/^([aeiouy])(._)/, "$1$2way")
-//     .replace(/^(_[_^aeiouy]+)(._)/, "$2$1ay");
-// }
+function pigLatin(str) {
+  return str
+    .replace(/^([aeiouy])(._)/, "$1$2way")
+    .replace(/^(_[_^aeiouy]+)(._)/, "$2$1ay");
+}
 
 console.log(pigLatin("pig"), "igpay");
 console.log(pigLatin("glove"), "oveglay");
