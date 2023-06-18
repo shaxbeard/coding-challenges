004ea067fb1bc27bb5bc8b853548d8f747458df4
Eric Carlson <eric.carlson2972@gmail.com>
2023-06-18 06:05:43 -0400

    initial commit

---
+++ single-number.js
@@ -0,0 +1,34 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// Example 1:
+
+// Input: nums = [2,2,1]
+// Output: 1
+
+// Example 2:
+// Input: nums = [4,1,2,1,2]
+// Output: 4
+
+// Example 3:
+// Input: nums = [1]
+// Output: 1
+
+function singleNumber(nums) {
+  let numSet = new Set();
+
+  for (let num of nums) {
+    if (numSet.has(num)) {
+      numSet.delete(num);
+    } else {
+      numSet.add(num);
+    }
+  }
+
+  return numSet.values().next().value;
+}
+
+console.log(singleNumber([2, 2, 1]), 1);
+console.log(singleNumber([4, 1, 2, 1, 2]), 4);
+console.log(singleNumber([1]), 1);
