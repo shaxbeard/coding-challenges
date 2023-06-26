a3c25b11b1ba73720974bc40b9f322979499e800
Eric Carlson <eric.carlson2972@gmail.com>
2023-06-26 06:49:06 -0400

    Add recursive solution with closure

--- fibonacci-leet.js
+++ fibonacci-leet.js
@@ -29,15 +29,15 @@
 */
 
 //  FOR() LOOP SOLUTION
-function fib(n) {
-  const arr = [0, 1];
-  for (let i = 2; i <= n; i++) {
-    arr[i] = arr[i - 1] + arr[i - 2];
-  }
-  return arr[n];
-}
+// function fib(n) {
+//   const arr = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     arr[i] = arr[i - 1] + arr[i - 2];
+//   }
+//   return arr[n];
+// }
 
-console.log(fib(6), 8);
+// console.log(fib(6), 8);
 //[0,1,1,2,3,5,8,13,21,34,55]
 
 // ITERATIVE SOLUTION WITH WHILE()
@@ -72,49 +72,7 @@ console.log(fib(6), 8);
 
 // [0,1,1,2,3,5]  # Do we include the zero or not?
 
-// CS DOJO RECURSIVE SOLUTION
-// function fib(n) {
-//   if (n === 1 || n === 2) {
-//     return 1;
-//   }
-//   return fib(n - 1) + fib(n - 2);
-// }
-// console.log(fib(4), 5);
-
-// [1,1,2,3,5]  # This starts the fibonacci sequence WITHOUT THE ZERO
-
-// RECURSIVE SOLUTION USING MEMOIZATION
-// function fib(n, memo) {
-//   memo = memo || {};
-//   if (memo[n]) {
-//     return memo[n];
-//   }
-
-//   if (n <= 1) {
-//     return 1;
-//   }
-
-//   return (memo[n] = fib(n - 1, memo) + fib(n - 2, memo));
-// }
-// console.log(fib(5), 8);
-
-// VISUALIZATION VERSION OF MEMOIZED FIBONACCI
-// var memo = {};
-// var fib = function (n) {
-//   if (n === 0 || n === 1) {
-//     return n;
-//   } else if (memo[n]) {
-//     return memo[n];
-//   } else {
-//     var result = fib(n - 1) + fib(n - 2);
-//     memo[n] = result;
-//     return result;
-//   }
-// };
-
-// fib(5);
-
-// LEON VERSION OF MEMOIZED FIBONACCI
+// LEON VERSION OF MEMOIZED FIBONACCI - memo in the global scope
 // const memo = {};
 // function fib(n) {
 //   if (n <= 1) {
@@ -130,16 +88,44 @@ console.log(fib(6), 8);
 // console.log(fib(5));
 // [0, 1, 1, 2, 3, 5, 8];
 
-// SIMPLE RECURSIVE SOLUTION
-// let memo = {};
-// function fib(n) {
-//   if (n <= 1) {
-//     return n;
-//   }
-//   let result = fib(n - 1) + fib(n - 2);
-//   memo[n] = result;
-//   return result;
-// }
-
 //log
 // console.log(fib(5));
+
+// RECURSIVE SOLUTION USING MEMOIZATION - memo in local scope, no closure
+// function fib(n, memo) {
+//   memo = memo || {};
+//   if (memo[n]) {
+//     return memo[n];
+//   }
+
+//   if (n <= 1) {
+//     return 1;
+//   }
+
+//   return (memo[n] = fib(n - 1, memo) + fib(n - 2, memo));
+// }
+// console.log(fib(6));
+
+//RECURSIVE SOLUTION USING MEMOIZATION AND CLOSURE
+function fibonacciClosure() {
+  let memo = {};
+  return function fib(n) {
+    if (n in memo) {
+      return memo[n];
+    } else {
+      if (n < 2) {
+        return 1; // OR return n to add zero at start
+      } else {
+        return (memo[n] = fib(n - 1) + fib(n - 2));
+      }
+    }
+  };
+}
+
+const fasterFib = fibonacciClosure();
+
+console.log(fasterFib(5), 8);
+
+//[0,1,1,2,3,5,8,13,21,34,55]
+//OR
+//[1,1,2,3,5,8,13,21,34,55]
