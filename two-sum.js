95cb54a2ab4b73704ef7a2205b0f0841d7fe872d
Eric Carlson <eric.carlson2972@gmail.com>
2023-06-28 06:29:04 -0400

    Add some comments for the hash map solution

--- two-sum.js
+++ two-sum.js
@@ -5,7 +5,7 @@
 // You can return the answer in any order.
 
 // Inputs are
-// #1 an array of integers
+// #1 an array of integers # NOT NECESSARILY SORTED IN THIS CASE
 // #2 an integer target
 // Return the indices of the two ints whose sum = target
 
@@ -37,35 +37,39 @@
 //   return [];
 // }
 
-// Linear time complexity - map the indexes and values
-// function twoSum(nums, target) {
-// //   // create map of the indexes and values
-//     const map = {};
-//     for (let i = 0; i < nums.length; i++) {
-//         map[i] = nums[i]; // map = { 0: 2, 1: 7, 2: 11, 3: 15}
-//     }
-// //   // loop over the map
-//     for (let j = 0; j < map.length; j++) {
-//         if (map)
-//     }
-// //   // if two values have sum of target, then return their keys
-// }
-
 // Linear time complexity O(n) - You calculate the sum AS YOU ARE BUILDING THE MAP
 function twoSum(nums, target) {
+  // Create an empty object to store the numbers in the input array as keys and their indices as values.
   const numMap = {};
+  debugger;
 
   for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-    if (complement in numMap) {
-      return [numMap[complement], i];
+    // Check if the difference between the target value and the current number is already in the numMap object.
+    if (target - nums[i] in numMap) {
+      // If it is, return an array containing the index of the current number and the index of the number in numMap that adds up to the target value.
+      return [numMap[target - nums[i]], i];
     }
+    // If the difference is not in the numMap object, add the current number and its index to the object.
     numMap[nums[i]] = i;
   }
-
   return [];
 }
 
-console.log(twoSum([2, 7, 11, 15], 9), [0, 1]);
+// console.log(twoSum([2, 7, 11, 15], 9), [0, 1]);
 console.log(twoSum([3, 2, 4], 6), [1, 2]);
 console.log(twoSum([3, 3], 6), [0, 1]);
+
+// var twoSum = function (nums, target) {
+//   let map = new Map();
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (map.has(target - nums[i])) {
+//       return [map.get(target - nums[i]), i];
+//     } else {
+//       map.set(nums[i], i);
+//     }
+//   }
+//   return [];
+// };
+// console.log(twoSum([3, 2, 4], 6), [1, 2]);
+// console.log(twoSum([3, 3], 6), [0, 1]);
