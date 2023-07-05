b651b2c478d34fead4687652d0ca7087f2aa5d8c
Eric Carlson <eric.carlson2972@gmail.com>
2023-07-05 06:54:06 -0400

    aded new solution

--- combining-arrays-without-duplicates-ultimate.js
+++ combining-arrays-without-duplicates-ultimate.js
@@ -1,9 +1,11 @@
-function mergeArrays(...arrays) {
-  let jointArray = [];
+// function mergeArrays(...arrays) {
+//   let jointArray = [];
 
-  arrays.forEach(array => (jointArray = [...jointArray, ...array]));
+//   arrays.forEach(array => (jointArray = [...jointArray, ...array]));
+//   console.log(jointArray);
 
-  return [...new Set([...jointArray])];
-}
+//   // return [...new Set([...jointArray])];
+//   return [...new Set(jointArray)];
+// }
 
-console.log(mergeArrays([1, 2], [2, 3], [3, 4], [4, 5]));
+// console.log(mergeArrays([1, 2], [2, 3], [3, 4], [4, 5]));
