b1fa5b76673b802186ba01e07f5a1c3f9a888fd4
Eric Carlson <eric.carlson2972@gmail.com>
2023-07-22 06:03:51 -0400

    new solution added

--- first-recurring-character.js
+++ first-recurring-character.js
@@ -1,6 +1,6 @@
 // Google question
-//Given an array = [2,5,1,2,3,5,1,2,4]:
-//It should return 2
+// Given an array = [2,5,1,2,3,5,1,2,4]:
+// It should return 2
 
 // Given an array = [2,1,1,2,3,5,1,2,4]:
 // It should return 1
@@ -12,8 +12,9 @@
 // The nested loop will run twice, but the first time it will return 5.
 // The hash map will return
 
-//SOLUTION WITH NESTED LOOPS - O(n^2)
+// SOLUTION WITH NESTED LOOPS - O(n^2)
 // function firstRecurring(arr) {
+//   debugger;
 //   for (let i = 0; i < arr.length; i++) {
 //     for (let j = i + 1; j < arr.length; j++) {
 //       if (arr[i] === arr[j]) {
@@ -24,25 +25,26 @@
 //   return undefined;
 // }
 // console.log(firstRecurring([2, 5, 1, 2, 3, 5, 1, 2, 4]), 2);
-// console.log(firstRecurring([2, 1, 1, 2, 3, 5, 1, 2, 4]), 1);
+// console.log(firstRecurring([2, 1, 1, 2, 3, 5, 1, 2, 4]), 1); // This one fails
 // console.log(firstRecurring([2, 3, 4, 5]), undefined);
 // console.log(firstRecurring([2, 5, 5, 2, 3, 5, 1, 2, 4]), 2);
 
-//SOLUTION WITH MAP ONLY - NO HASH, NO TABLE - O(n) time and O(n) space (because we are storing the values)
-// function firstRecurring(arr) {
-//   let map = {};
-//   for (let num of arr) {
-//     if (map[num]) {
-//       return num;
-//     } else {
-//       map[num] = true;
-//     }
-//   }
-//   return undefined;
-// }
+// SOLUTION WITH MAP ONLY - NO HASH, NO TABLE - O(n) time and O(n) space (because we are storing the values)
+function firstRecurring(arr) {
+  let map = {};
+  debugger;
+  for (let num of arr) {
+    if (map[num]) {
+      return num;
+    } else {
+      map[num] = true;
+    }
+  }
+  return undefined;
+}
 
 // console.log(firstRecurring([2, 5, 1, 2, 3, 5, 1, 2, 4]), 2);
-// console.log(firstRecurring([2, 1, 1, 2, 3, 5, 1, 2, 4]), 1);
+console.log(firstRecurring([2, 1, 1, 2, 3, 5, 1, 2, 4]), 1);
 // console.log(firstRecurring([2, 3, 4, 5]), undefined);
 // console.log(firstRecurring([2, 5, 5, 2, 3, 5, 1, 2, 4]), 5);
 //
