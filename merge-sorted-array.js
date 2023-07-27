9cb7b9c461b515a65f988bd712f8b3bdb50c9587
Eric Carlson <eric.carlson2972@gmail.com>
2023-07-27 06:42:24 -0400

    initial commit

--- merge-sorted-array.js
+++ merge-sorted-array.js
@@ -1,3 +1,57 @@
+// First, a simpler version of merging two arrays, then below it, the Leetcode version of the question
+
+function mergeSortedArrays(array1, array2) {
+  const mergedArray = [];
+  let array1Item = array1[0];
+  let array2Item = array2[0];
+  let i = 1;
+  let j = 1;
+
+  while (array1Item || array2Item) {
+    if (!array2Item || array1Item < array2Item) {
+      mergedArray.push(array1Item);
+      array1Item = array1[i];
+      i++;
+    } else {
+      mergedArray.push(array2Item);
+      array2Item = array2[j];
+      j++;
+    }
+  }
+  return mergedArray;
+}
+
+console.log(
+  mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]),
+  [0, 3, 4, 4, 6, 30, 31]
+);
+
+// function mergeSortedArrays(array1, array2) {
+//   const mergedArray = [];
+//   let array1Item = array1[0];
+//   let array2Item = array2[0];
+//   let i = 1;
+//   let j = 1;
+
+//   while (array1Item !== undefined || array2Item !== undefined) {
+//     if (array2Item === undefined || array1Item < array2Item) {
+//       mergedArray.push(array1Item);
+//       array1Item = array1[i];
+//       i++;
+//     } else {
+//       mergedArray.push(array2Item);
+//       array2Item = array2[j];
+//       j++;
+//     }
+//   }
+//   return mergedArray;
+// }
+
+// console.log(
+//   mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]),
+//   [0, 3, 4, 4, 6, 30, 31]
+// );
+
 // You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
 // and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
 
@@ -26,34 +80,34 @@
 // The result of the merge is [1].
 // Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
 
-function merge(nums1, m, nums2, n) {
-  let p1 = m - 1; // pointer for nums1
-  let p2 = n - 1; // pointer for nums2
-  let p = m + n - 1; // pointer for merged array
+// function merge(nums1, m, nums2, n) {
+//   let p1 = m - 1; // pointer for nums1
+//   let p2 = n - 1; // pointer for nums2
+//   let p = m + n - 1; // pointer for merged array
 
-  while (p1 >= 0 && p2 >= 0) {
-    if (nums1[p1] > nums2[p2]) {
-      nums1[p] = nums1[p1];
-      p1--;
-    } else {
-      nums1[p] = nums2[p2];
-      p2--;
-    }
-    p--;
-  }
+//   while (p1 >= 0 && p2 >= 0) {
+//     if (nums1[p1] > nums2[p2]) {
+//       nums1[p] = nums1[p1];
+//       p1--;
+//     } else {
+//       nums1[p] = nums2[p2];
+//       p2--;
+//     }
+//     p--;
+//   }
 
-  // If there are any remaining elements in nums2, copy them to nums1
-  while (p2 >= 0) {
-    nums1[p] = nums2[p2];
-    p2--;
-    p--;
-  }
-}
+//   // If there are any remaining elements in nums2, copy them to nums1
+//   while (p2 >= 0) {
+//     nums1[p] = nums2[p2];
+//     p2--;
+//     p--;
+//   }
+// }
 
-let nums1 = [1, 2, 3, 0, 0, 0];
-let m = 3;
-let nums2 = [2, 5, 6];
-let n = 3;
+// let nums1 = [1, 2, 3, 0, 0, 0];
+// let m = 3;
+// let nums2 = [2, 5, 6];
+// let n = 3;
 
-merge(nums1, m, nums2, n);
-console.log(nums1); // Output: [1, 2, 2, 3, 5, 6]
+// merge(nums1, m, nums2, n);
+// console.log(nums1); // Output: [1, 2, 2, 3, 5, 6]
