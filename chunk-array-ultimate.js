228e3ff5d2c3d75b5c40b5eefad5bf9e274e1c8f
Eric Carlson <eric.carlson2972@gmail.com>
2023-08-25 09:43:45 -0400

    add new solution

--- chunk-array-ultimate.js
+++ chunk-array-ultimate.js
@@ -21,7 +21,11 @@ function chunkArray(arr, size) {
   return result;
 }
 
-console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5));
+console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5), [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13],
+]);
 
 // LOOPING THROUGH THE NUMBER OF CHUNKS WITH SPLICE()
 // function chunkArray(array, size) {
