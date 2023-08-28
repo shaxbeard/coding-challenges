b31610c203b95392eeabc7ede2edd20c47fb59bb
Eric Carlson <eric.carlson2972@gmail.com>
2023-08-28 06:35:58 -0400

    add a solution and time complexitiy

--- remove-duplicate-strings-from-nested-arrays.js
+++ remove-duplicate-strings-from-nested-arrays.js
@@ -1,22 +1,78 @@
-const arrayOfArrays = [
-  ["apple", "banana", "pear", "orange"],
-  ["apple", "pear", "kiwi", "strawberry"],
-  ["apple", "pear", "pear", "pear", "pear", "grape"],
-];
+//Remove any duplicated strings within each of the nested arrays.
 
-// Remove duplicates within each subarray
-const uniqueArrays = arrayOfArrays.map(subarray => {
+//Example
+// Input: Some of the nested arrays have duplicate strings
+// [
+//   ["apple", "banana", "pear", "orange"],
+//   ["apple", "pear", "kiwi", "kiwi", "strawberry"],
+//   ["apple", "pear", "pear", "pear", "pear", "grape"],
+// ]
+// Output: Each array has only unique strings
+// [
+//   [ 'apple', 'banana', 'pear', 'orange' ],
+//   [ 'apple', 'pear', 'kiwi', 'strawberry' ],
+//   [ 'apple', 'pear', 'grape' ]
+// ]
+
+// SOLUTION USING MAP() AND A SET
+// function uniqueArrays(arrayOfArrays) {
+//   return arrayOfArrays.map(subarray => [...new Set(subarray)]);
+// }
+
+// Nested loops O(n * m) - n = nested arrays, m = items in each array
+// function uniqueArrays(arrayOfArrays) {
+//   const uniques = arrayOfArrays.map(subarray => {
+//     const occurrences = {};
+//     const uniqueSubarray = [];
+
+//     for (let item of subarray) {
+//       if (!occurrences[item]) {
+//         occurrences[item] = true;
+//         uniqueSubarray.push(item);
+//       }
+//     }
+//     return uniqueSubarray;
+//   });
+
+//   return uniques;
+// }
+
+// function uniqueArrays(arrayOfArrays) {
+//   const uniques = arrayOfArrays.map(subarray => {
+//     const occurrences = {};
+//     const uniqueSubarray = [];
+
+//     for (let item of subarray) {
+//       if (!uniqueSubarray.includes(item)) {
+//         uniqueSubarray.push(item);
+//       }
+//     }
+//     return uniqueSubarray;
+//   });
+
+//   return uniques;
+// }
+
+function uniqueArrays(arrayOfArrays) {
   const occurrences = {};
   const uniqueSubarray = [];
 
-  for (let item of subarray) {
-    if (!occurrences[item]) {
-      occurrences[item] = true;
-      uniqueSubarray.push(item);
-    }
-  }
+  arrayOfArrays;
+}
 
-  return uniqueSubarray;
-});
+// function uniqueArrays(arrayOfArrays) {
+//   return arrayOfArrays.map(subarray => [...new Set(subarray)]);
+// }
 
-console.log(uniqueArrays);
+console.log(
+  uniqueArrays([
+    ["apple", "banana", "pear", "orange"],
+    ["apple", "pear", "kiwi", "kiwi", "strawberry"],
+    ["apple", "pear", "pear", "pear", "pear", "grape"],
+  ]),
+  [
+    ["apple", "banana", "pear", "orange"],
+    ["apple", "pear", "kiwi", "strawberry"],
+    ["apple", "pear", "grape"],
+  ]
+);
