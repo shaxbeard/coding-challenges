0ad8a0c44c2feca25b9b7987ac8ed0c138e73f3e
Eric Carlson <eric.carlson2972@gmail.com>
2023-09-16 06:23:52 -0400

    add another solution

--- valid-parentheses-leet.js
+++ valid-parentheses-leet.js
@@ -25,47 +25,47 @@
 // 1 <= s.length <= 104
 // s consists of parentheses only '()[]{}'.
 
-const isValid = s => {
-  let stack = [];
-  for (let i = 0; i < s.length; i++) {
-    let char = stack[stack.length - 1];
-    if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
-      stack.push(s[i]);
-    } else if (
-      (char == "(" && s[i] == ")") ||
-      (char == "{" && s[i] == "}") ||
-      (char == "[" && s[i] == "]")
-    ) {
-      stack.pop();
-    } else return false;
-  }
-  return stack.length ? false : true;
-};
-
-console.log(isValid("()[]{}"), true);
-// console.log(isValid("()[{}"), false);
-// console.log(isValid("([]{})"), true);
-// console.log(isValid("([{}])"), true);
-
 // const isValid = s => {
-//   const map = {
-//     ")": "(",
-//     "}": "{",
-//     "]": "[",
-//   };
-//   const stack = [];
+//   let stack = [];
 //   for (let i = 0; i < s.length; i++) {
 //     let char = stack[stack.length - 1];
 //     if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
 //       stack.push(s[i]);
-//     } else if (char === map[s[i]]) {
+//     } else if (
+//       (char == "(" && s[i] == ")") ||
+//       (char == "{" && s[i] == "}") ||
+//       (char == "[" && s[i] == "]")
+//     ) {
 //       stack.pop();
 //     } else return false;
 //   }
 //   return stack.length ? false : true;
 // };
 
-// console.log(isValid("[[{({{[[()]]}})}]]"));
+// console.log(isValid("()[]{}"), true);
+// console.log(isValid("()[{}"), false);
+// console.log(isValid("([]{})"), true);
+// console.log(isValid("([{}])"), true);
+
+const isValid = s => {
+  const map = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    let char = stack[stack.length - 1];
+    if (s[i] == "(" || s[i] == "{" || s[i] == "[") {
+      stack.push(s[i]);
+    } else if (char === map[s[i]]) {
+      stack.pop();
+    } else return false;
+  }
+  return stack.length ? false : true;
+};
+
+console.log(isValid("[[{({{[[()]]}})}]]"));
 
 // console.log(isValid("()[]{}"), true);
 // console.log(isValid("()[{}"), false);
