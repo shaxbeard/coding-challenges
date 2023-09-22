672bb7d8e02566228160ecc629cf998cdd082bd2
Eric Carlson <eric.carlson2972@gmail.com>
2023-09-22 06:41:00 -0400

    new solution added

--- string-ends-with-str-7str.js
+++ string-ends-with-str-7str.js
@@ -9,13 +9,27 @@
 //Desired result
 //solution('abc', 'bc') -> true
 //solution('abc', 'd') -> false
-
 //solution('abcdefgh', 'fgh') -> true
 
-//LOL - I guess I should look at the list of JS string methods first
+// METHOD #1 - USING A BUILT-IN STRING METHOD SPECIFICALLY FOR THIS USAGE
+// function solution(str, ending) {
+//   return str.endsWith(ending);
+// }
 
+// METHOD #2 - ADAPTING THE SLICE() METHOD - AS IF THE ENDSWITH() METHOD DIDN'T EXIST
+// function solution(str, ending) {
+//   var len = ending.length;
+//   var str = str.slice(-len);
+//   return str.match(ending) ? true : false;
+// }
+
+// METHOD #2 - ADAPTING THE SLICE() METHOD - AS IF THE ENDSWITH() METHOD DIDN'T EXIST
 function solution(str, ending) {
-  return str.endsWith(ending);
+  var len = ending.length;
+  var str = str.slice(-len);
+  return str.search(ending) === -1 ? false : true;
 }
 
-console.log(solution("abc", "bc"));
+console.log(solution("abc", "bc"), true);
+console.log(solution("abc", "zzz"), false);
+console.log(solution("abcdefgh", "fgh"), true);
