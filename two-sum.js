cf39421376d0330fe7f2eadd5f67227782db38ed
Eric Carlson <eric.carlson2972@gmail.com>
2023-09-27 07:44:34 -0400

    new solution added

--- two-sum.js
+++ two-sum.js
@@ -43,27 +43,27 @@
 // }
 
 // Linear time complexity O(n) - You calculate the sum AS YOU ARE BUILDING THE MAP
-// function twoSum(nums, target) {
-//   // Create an empty object to store the numbers in the input array as keys and their indices as values.
-//   const numMap = {};
-//   debugger;
+function twoSum(nums, target) {
+  // Create an empty object to store the numbers in the input array as keys and their indices as values.
+  const numMap = {};
+  // debugger;
 
-// Loop through each number in the array  - we are NOT looping through the numMap
-//   for (let i = 0; i < nums.length; i++) {
-//     // Check if the difference between the target value and the current ARRAY number is already a KEY in the numMap object.
-//     if (target - nums[i] in numMap) {
-//       // If it is, return an array containing the index of the current number and the index of the number in numMap that adds up to the target value.
-//       return [numMap[target - nums[i]], i];
-//     }
-//     // If the difference is not in the numMap object, add the current number and its index to the object.
-//     numMap[nums[i]] = i;
-//   }
-//   return [];
-// }
+  // Loop through each number in the array  - we are NOT looping through the numMap
+  for (let i = 0; i < nums.length; i++) {
+    // Check if the difference between the target value and the current ARRAY number is already a KEY in the numMap object.
+    if (target - nums[i] in numMap) {
+      // If it is, return an array containing the index of the current number and the index of the number in numMap that adds up to the target value.
+      return [numMap[target - nums[i]], i];
+    }
+    // If the difference is not in the numMap object, add the current number and its index to the object.
+    numMap[nums[i]] = i;
+  }
+  return [];
+}
 
-// console.log(twoSum([2, 7, 11, 15], 9), [0, 1]);
-// console.log(twoSum([3, 2, 4], 6), [1, 2]);
-// console.log(twoSum([3, 3], 6), [0, 1]);
+console.log(twoSum([2, 7, 11, 15], 9), [0, 1]);
+console.log(twoSum([3, 2, 4], 6), [1, 2]);
+console.log(twoSum([3, 3], 6), [0, 1]);
 
 // var twoSum = function (nums, target) {
 //   let map = new Map();
@@ -103,32 +103,32 @@
 
 //Brute force with nested arrays will be O(n2)
 //Using a map to store the numbers and their indices should be O(n)
-function twoSum(nums, target) {
-  //Check input?
-  //Initialize an empty object to store each number and its index { 3: 0, 2: 1, 4: 2}
-  let map = {};
+// function twoSum(nums, target) {
+//   //Check input?
+//   //Initialize an empty object to store each number and its index { 3: 0, 2: 1, 4: 2}
+//   let map = {};
 
-  //Loop through the array and check if the current number plus its "complement" (in the map) equals the target
+//   //Loop through the array and check if the current number plus its "complement" (in the map) equals the target
 
-  for (let [i, num] of nums.entries()) {
-    // for (let i = 0; i < nums.length; i++) {
-    // [3, 2, 4]
-    //The complement is the target minus the current number
-    let complement = target - num;
+//   for (let [i, num] of nums.entries()) {
+//     // for (let i = 0; i < nums.length; i++) {
+//     // [3, 2, 4]
+//     //The complement is the target minus the current number
+//     let complement = target - num;
 
-    //If the complement IS in the map already, return the INDICES of the current num and the complement
+//     //If the complement IS in the map already, return the INDICES of the current num and the complement
 
-    if (complement in map) {
-      return [map[complement], i];
-    }
+//     if (complement in map) {
+//       return [map[complement], i];
+//     }
 
-    //If the complement is NOT in the map, add the current number and its index to the map
-    map[num] = i;
-  }
+//     //If the complement is NOT in the map, add the current number and its index to the map
+//     map[num] = i;
+//   }
 
-  //If there are no pairs that add up to the target, return -1?
-  return [];
-}
+//   //If there are no pairs that add up to the target, return -1?
+//   return [];
+// }
 
-console.log(twoSum([3, 2, 4], 6), [1, 2]);
-console.log(twoSum([3, 3], 6), [0, 1]);
+// console.log(twoSum([3, 2, 4], 6), [1, 2]);
+// console.log(twoSum([3, 3], 6), [0, 1]);
