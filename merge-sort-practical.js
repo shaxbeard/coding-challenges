f7ebd52872e77eff06c6642f7bf59d11a65feebe
Eric Carlson <eric.carlson2972@gmail.com>
2023-09-28 06:04:30 -0400

    tweak the merge function as part of merge sort

--- merge-sort-practical.js
+++ merge-sort-practical.js
@@ -1,18 +1,3 @@
-// Split the array into halves and merge them recursively
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    // return once we hit an array with a single item
-    return arr;
-  }
-
-  const middle = Math.floor(arr.length / 2); // get the middle item of the array rounded down
-  const left = arr.slice(0, middle); // items on the left side
-  const right = arr.slice(middle); // items on the right side
-  const sortedLeft = mergeSort(left);
-  const sortedRight = mergeSort(right);
-  return merge(sortedLeft, sortedRight);
-}
-
 // compare the arrays item by item and return the concatenated result
 function merge(left, right) {
   let result = [];
@@ -30,6 +15,21 @@ function merge(left, right) {
   }
 
   return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+}
+
+// Split the array into halves and merge them recursively
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    // return once we hit an array with a single item
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2); // get the middle item of the array rounded down
+  const left = arr.slice(0, middle); // items on the left side
+  const right = arr.slice(middle); // items on the right side
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+  return merge(sortedLeft, sortedRight);
 }
 
 const list = [2, 5, 1, 3, 7, 2, 3, 8, 6, 3];
