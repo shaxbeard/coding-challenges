e659b57c25e8a1b42cab793523d852be9a87450c
Eric Carlson <eric.carlson2972@gmail.com>
2023-10-28 06:31:54 -0400

    new solution added

--- binary-search-pratical.js
+++ binary-search-pratical.js
@@ -26,7 +26,6 @@
 
 // BINARY SEARCH SOLUTION #1
 // function binarySearch(arr, item) {
-//   debugger;
 //   var min = 0;
 //   var max = arr.length - 1;
 //   var middle;
@@ -47,30 +46,38 @@
 //   return -1;
 // }
 
-function binarySearch(arr, item) {
-  // min, max, and middle indexes
-  let min = 0;
-  let max = arr.length - 1;
-  let middle;
+// David Malan's pseudocode step #1 for Binary Search
+//if "target" is in the middle
+// return target
+//Else if target < middle
+// search the left half
+//Else if target > middle
+// search the right half
 
-  //while min <= max do the binary searching
-  while (min <= max) {
-    // calculate the middle index - WHERE DOES THIS GO?
-    middle = Math.floor((min + max) / 2);
-    // if the middle IS the item, return the index
-    if (arr[middle] === item) {
-      return middle;
-    }
-    // else, adjust the min or the max
-    if (arr[middle] < item) {
-      min = middle + 1;
-    } else {
-      max = middle - 1;
-    }
-  }
-  return -1;
-}
-console.log(binarySearch([2, 6, 64, 90, 103, 200], 90), 3);
+// function binarySearch(arr, item) {
+//   // min, max, and middle indexes
+//   let min = 0;
+//   let max = arr.length - 1;
+//   let middle;
+
+//   //while min <= max do the binary searching
+//   while (min <= max) {
+//     // calculate the middle index - WHERE DOES THIS GO?
+//     middle = Math.floor((min + max) / 2);
+//     // if the middle IS the item, return the index
+//     if (arr[middle] === item) {
+//       return middle;
+//     }
+//     // else, adjust the min or the max
+//     if (arr[middle] < item) {
+//       min = middle + 1;
+//     } else {
+//       max = middle - 1;
+//     }
+//   }
+//   return -1;
+// }
+// console.log(binarySearch([2, 6, 64, 90, 103, 200], 90), 3);
 
 // LEON'S SOLUTION
 // function binarySearch(nums, target) {
