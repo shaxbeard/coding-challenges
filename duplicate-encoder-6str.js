d3aeca3f680b889cb1767147b62c41bb47262ea4
Eric Carlson <eric.carlson2972@gmail.com>
2023-11-08 16:30:34 -0500

    Add new solution referencing the third array parameter in map

--- duplicate-encoder-6str.js
+++ duplicate-encoder-6str.js
@@ -5,67 +5,69 @@
 
 // Ignore capitalization when determining if a character is a duplicate.
 
-//input - string, uppercase and lowercase, numbers? no, special characters-> yes, spaces->yes
-//return open paren ( if the character appears just once, return ) if the character appears 1+ in the str,
-// ignore case!!
-
 // "din"      =>  "((("
 // "recede"   =>  "()()()"
 // "Success"  =>  ")())())"
 // "(( @"     =>  "))(("
 
+// function duplicateEncoder(str) {
+//to ignore case, convert toLowerCase at the start
+//determine if each character is unique or duplicate
+//To loop over each character in this string for the comparison, I need to convert to array
+//TEST -> if indexOf each char === lastIndexOf each char -> (  : )
+/* *************************** */
+/* *************************** */
+/* *************************** */
+// //THIS SOLUTION USES TWO COPIES OF THE PARAMETER
+// const lowerArr = str.toLowerCase();
+// const strLower = str.toLowerCase();
+// return lowerArr
+//   .split("")
+//   .map(l => (strLower.indexOf(l) === strLower.lastIndexOf(l) ? "(" : ")"))
+//   .join("");
+
+/* *************************** */
+/* *************************** */
+/* *************************** */
+
+//   //THIS IS THE BEST QUADRATIC SOLUTION
+//   // toLowerCase -> split -> map -> join
+//   // if letter is unique, then first index === last index; else, letter has duplicates
 function duplicateEncoder(str) {
-  //to ignore case, convert toLowerCase at the start
-  //determine if each character is unique or duplicate
-  //To loop over each character in this string for the comparison, I need to convert to array
-  //TEST -> if indexOf each char === lastIndexOf each char -> (  : )
-  /* *************************** */
-  /* *************************** */
-  /* *************************** */
-  // //THIS SOLUTION USES TWO COPIES OF THE PARAMETER
-  // const lowerArr = str.toLowerCase();
-  // const strLower = str.toLowerCase();
-  // return lowerArr
-  //   .split("")
-  //   .map(l => (strLower.indexOf(l) === strLower.lastIndexOf(l) ? "(" : ")"))
-  //   .join("");
-
-  /* *************************** */
-  /* *************************** */
-  /* *************************** */
-
-  //   //THIS IS THE BEST QUADRATIC SOLUTION
-  //   // toLowerCase -> split -> map -> join
-  //   // if letter is unique, then first index === last index; else, letter has duplicates
-  //   return str
-  //     .toLowerCase()
-  //     .split("")
-  //     .map((l, i, a) => (a.indexOf(l) === a.lastIndexOf(l) ? "(" : ")"))
-  //     .join("");
-
-  // // THIS IS MY LINEAR SOLUTION
-  // const arr = str.toLowerCase().split("");
-  // const letterCounts = arr.reduce((total, letter) => {
-  //   total[letter] == null ? (total[letter] = 1) : total[letter]++;
-  //   return total;
-  // }, {});
-  // return arr.map(letter => (letterCounts[letter] > 1 ? ")" : "(")).join("");
-
-  //THIS IS A MEMO SOLUTION USING TWO FUNCTIONS FROM CODEWARS
-  function countCharacters(chars) {
-    return chars.reduce((memo, char) => {
-      memo[char] = memo[char] ? memo[char] + 1 : 1;
-      return memo;
-    }, {});
-  }
-  function duplicateEncode(word) {
-    const chars = word.split("").map(ch => ch.toLowerCase());
-    const charsCount = countCharacters(chars);
-    return chars.map(ch => (charsCount[ch] > 1 ? ")" : "(")).join("");
-  }
-  return duplicateEncode(str);
+  return str
+    .toLowerCase()
+    .split("")
+    .map((l, i, a) => (a.indexOf(l) === a.lastIndexOf(l) ? "(" : ")"))
+    .join("");
 }
 
 console.log(duplicateEncoder("din"), "(((");
 console.log(duplicateEncoder("recede"), "()()()");
 console.log(duplicateEncoder("Success"), ")())())");
+
+// // THIS IS MY LINEAR SOLUTION
+// const arr = str.toLowerCase().split("");
+// const letterCounts = arr.reduce((total, letter) => {
+//   total[letter] == null ? (total[letter] = 1) : total[letter]++;
+//   return total;
+// }, {});
+// return arr.map(letter => (letterCounts[letter] > 1 ? ")" : "(")).join("");
+
+//THIS IS A MEMO SOLUTION USING TWO FUNCTIONS FROM CODEWARS
+//   function countCharacters(chars) {
+//     return chars.reduce((memo, char) => {
+//       memo[char] = memo[char] ? memo[char] + 1 : 1;
+//       return memo;
+//     }, {});
+//   }
+//   function duplicateEncode(word) {
+//     const chars = word.split("").map(ch => ch.toLowerCase());
+//     const charsCount = countCharacters(chars);
+//     return chars.map(ch => (charsCount[ch] > 1 ? ")" : "(")).join("");
+//   }
+//   return duplicateEncode(str);
+// }
+
+// console.log(duplicateEncoder("din"), "(((");
+// console.log(duplicateEncoder("recede"), "()()()");
+// console.log(duplicateEncoder("Success"), ")())())");
