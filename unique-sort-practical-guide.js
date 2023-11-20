d15658055c066f4993ccce1a34d756fae9214e6e
Eric Carlson <eric.carlson2972@gmail.com>
2023-11-20 07:03:14 -0500

    Add a new solution to the problem

--- unique-sort-practical-guide.js
+++ unique-sort-practical-guide.js
@@ -1,13 +1,43 @@
 //Task: Transform this simple sorting algorithm into a unique sort.
 // It should not return any duplicate values in the sorted array.
 
+//Input - array of integers, no funny business, but there will duplicates of some integers
+//Return - remove all duplicate integers, then sort the array
+
 //input: [1,5,2,1] => output: [1,2,5]
 //input: [4,2,2,3,2,2,2] => output: [2,3,4]
 
+// I COULD use nested arrays to remove the duplicatees
+// OR I could use a cache to check each number against a saved object of previous numbers
+
+// const uniqSort = function (arr) {
+//   //   const breadcrumbs = {};
+//   let uniques = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (i !== j && arr[i] !== arr[j]) {
+//         uniques.push(arr[i]);
+//       }
+//     }
+//     return uniques;
+//   }
+
+//   //   return arr.sort((a, b) => a - b);
+// };
+
 const uniqSort = function (arr) {
   const breadcrumbs = {};
+  const result = [arr[0]];
 
-  return arr.sort((a, b) => a - b);
+  for (let i = 1; i < arr.length; i++) {
+    if (!breadcrumbs[arr[i]]) {
+      result.push(arr[i]);
+      breadcrumbs[arr[i]] = true;
+    }
+  }
+
+  return result.sort((a, b) => a - b);
 };
 
-uniqSort([4, 2, 2, 3, 2, 2, 2]); // => [2,3,4]
+console.log(uniqSort([4, 2, 2, 3, 2, 2, 2]), [2, 3, 4]);
