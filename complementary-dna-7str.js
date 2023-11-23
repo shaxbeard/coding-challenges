a18d294c2213d484207be03bac632d8b6e2d88b8
Eric Carlson <eric.carlson2972@gmail.com>
2023-11-23 06:05:50 -0500

    add a new solution to the problem

--- complementary-dna-7str.js
+++ complementary-dna-7str.js
@@ -14,41 +14,41 @@
 
 //Method #1 - Simple conditional or switch statement
 
-// function DNAStrand(dna) {
-//   // input check
-//   if (dna.length < 1 || typeof dna !== "string") {
-//     return 0;
-//   }
+function DNAStrand(dna) {
+  // input check
+  if (dna.length < 1 || typeof dna !== "string") {
+    return 0;
+  }
 
-//   let dnaConverted = "";
-//   // Loop over the string and swap each letter as appropriate
-//   for (let char of dna) {
-//     if (char === "A") {
-//       dnaConverted += "T";
-//     }
-//     if (char === "T") {
-//       dnaConverted += "A";
-//     }
-//     if (char === "G") {
-//       dnaConverted += "C";
-//     }
-//     if (char === "C") {
-//       dnaConverted += "G";
-//     }
-//   }
-//   return dnaConverted;
-// }
+  let dnaConverted = "";
+  // Loop over the string and swap each letter as appropriate
+  for (let char of dna) {
+    if (char === "A") {
+      dnaConverted += "T";
+    }
+    if (char === "T") {
+      dnaConverted += "A";
+    }
+    if (char === "G") {
+      dnaConverted += "C";
+    }
+    if (char === "C") {
+      dnaConverted += "G";
+    }
+  }
+  return dnaConverted;
+}
 
 //Method #2 - Use a "map" to lookup the conversion for each letter
 
-// var pairs = { A: "T", T: "A", C: "G", G: "C" };
+var pairs = { A: "T", T: "A", C: "G", G: "C" };
 
-// function DNAStrand(dna) {
-//   return dna
-//     .split("")
-//     .map(char => pairs[char])
-//     .join("");
-// }
+function DNAStrand(dna) {
+  return dna
+    .split("")
+    .map(char => pairs[char])
+    .join("");
+}
 
 // Method #3 - Use replace() to a global find/replace on the letters
 
