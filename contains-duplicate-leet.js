e3f987dde5bcf953d9c5e36da3e838d8b7f45a30
Eric Carlson <eric.carlson2972@gmail.com>
2023-11-24 06:26:33 -0500

    add better time complexity solution

--- contains-duplicate-leet.js
+++ contains-duplicate-leet.js
@@ -79,9 +79,14 @@
 // console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]), true);
 
 // // USING SET
+// const containsDuplicate = function (nums) {
+//   const uniques = [...new Set(nums)];
+//   return uniques.length !== nums.length;
+// };
+
 const containsDuplicate = function (nums) {
-  const uniques = [...new Set(nums)];
-  return uniques.length !== nums.length;
+  const uniques = new Set(nums);
+  return uniques.size !== nums.length;
 };
 
 console.log(containsDuplicate([1, 2, 3, 1]), true);
