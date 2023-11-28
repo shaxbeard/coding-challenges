25bb3b714c0e90ace6dac23add4b54ea00157a24
Eric Carlson <eric.carlson2972@gmail.com>
2023-11-28 06:29:20 -0500

    new solution added to the problem

--- factorial.js
+++ factorial.js
@@ -1,15 +1,20 @@
 // USING A FOR LOOP
 
+function factorial(n) {
+  for (let i = n - 1; i > 1; i--) {
+    n *= i;
+  }
+  return n;
+}
 // function factorial(n) {
-//   if (n <= 1) {
-//     return 1;
+//   debugger;
+//   let answer = 1;
+//   for (let i = n; i > 1; i--) {
+//     answer *= i;
 //   }
-//   for (let i = n - 1; i > 1; i--) {
-//     n *= i;
-//   }
-//   return n;
+//   return answer;
 // }
-// console.log(factorial(4));
+console.log(factorial(5), 120);
 
 // USING A WHILE LOOP
 
@@ -49,3 +54,21 @@
 // }
 
 // console.log(factorial(5));
+
+// Recursive solution with memoization and closure
+// function factorial() {
+//   let memo = {};
+//   return function fact(num) {
+//     if (num < 2) {
+//       return 1;
+//     }
+//     if (memo[num]) {
+//       return memo[num];
+//     }
+//     return (memo[num] = num * fact(num - 1));
+//   };
+// }
+
+// const fasterFactorial = factorial();
+
+// console.log(fasterFactorial(5), 120);
