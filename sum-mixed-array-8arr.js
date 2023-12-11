71a729a683a627ad6711c593b80d45864c2e3d9b
Eric Carlson <eric.carlson2972@gmail.com>
2023-12-11 06:51:46 -0500

    add new solution with better time complexity

--- sum-mixed-array-8arr.js
+++ sum-mixed-array-8arr.js
@@ -1,5 +1,5 @@
 // Given an array of integers as strings and numbers,
-//return the sum of the array values as if all were numbers.
+// return the sum of the array values as if all were numbers.
 
 // Return your answer as a number.
 
@@ -10,8 +10,13 @@
 //Pseudocode -
 //Methods!
 
-function sumMix(x) {
-  return x.map(Number).reduce((total, item) => total + item, 0);
+function sumMix(arr) {
+  //Method #1 - the reduce() method with map(Number)
+  return arr.map(Number).reduce((sum, num) => sum + num, 0);
+
+  //Method #2 - the reduce() method with the unary operator
+  return arr.reduce((sum, num) => sum + +num, 0);
 }
 
-console.log(sumMix([1, 2, "3", "4"]));
+console.log(sumMix([1, 2, "3", "4"]), 10);
+console.log(sumMix(["5", "0", 2, "3", "4", 6]), 20);
