59388fe2ff196e0109fb9cc59034139d7b2e5ab9
Eric Carlson <eric.carlson2972@gmail.com>
2024-01-19 11:04:26 -0500

    new solution added

--- richest-customer-wealth.js
+++ richest-customer-wealth.js
@@ -48,6 +48,23 @@ function maximumWealth(accounts) {
   return maxWealthSoFar;
 }
 
+console.log(
+  maximumWealth([
+    [2, 8, 7],
+    [7, 1, 3],
+    [1, 9, 5],
+  ]),
+  17
+);
+
+// console.log(
+//   maximumWealth([
+//     [1, 2, 3],
+//     [3, 2, 1],
+//   ]),
+//   6
+// );
+
 // function maximumWealth(accounts) {
 //   let maxWealthSoFar = 0;
 
@@ -62,26 +79,26 @@ function maximumWealth(accounts) {
 //   return maxWealthSoFar;
 // }
 
-console.log(
-  maximumWealth([
-    [1, 2, 3],
-    [3, 2, 1],
-  ]),
-  6
-);
-console.log(
-  maximumWealth([
-    [1, 5],
-    [7, 3],
-    [3, 5],
-  ]),
-  10
-);
-console.log(
-  maximumWealth([
-    [2, 8, 7],
-    [7, 1, 3],
-    [1, 9, 5],
-  ]),
-  17
-);
+// console.log(
+//   maximumWealth([
+//     [1, 2, 3],
+//     [3, 2, 1],
+//   ]),
+//   6
+// );
+// console.log(
+//   maximumWealth([
+//     [1, 5],
+//     [7, 3],
+//     [3, 5],
+//   ]),
+//   10
+// );
+// console.log(
+//   maximumWealth([
+//     [2, 8, 7],
+//     [7, 1, 3],
+//     [1, 9, 5],
+//   ]),
+//   17
+// );
