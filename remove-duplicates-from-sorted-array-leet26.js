a6a11f5a6736e8423043a92582014219e3c59ff5
Eric Carlson <eric.carlson2972@gmail.com>
2024-01-22 06:33:26 -0500

    add an optimized solution

--- remove-duplicates-from-sorted-array-leet26.js
+++ remove-duplicates-from-sorted-array-leet26.js
@@ -23,11 +23,8 @@
 // Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
 
-var removeDuplicates = function (nums) {
-  //   return [...new Set(nums)];  // this does not remove the duplicates "in place"
-
-  // NESTED FOR LOOP (QUADRATIC - O(n^2)) - RUNTIME 144 ms
-  //Compare each individual number with the rest of the numbers in the array
+// // NESTED FOR LOOP (QUADRATIC - O(n^2)) - RUNTIME 144 ms
+// var removeDuplicates = function (nums) {
 //   for (let i = 0; i < nums.length; i++) {
 //     for (let j = i + 1; j < nums.length; j++) {
 //       // If a number is duplicated, remove the duplicate instances of the number from the array IN PLACE
@@ -38,24 +35,61 @@ var removeDuplicates = function (nums) {
 //     }
 //   }
 //   // Count the final number of unique elements (as k)
-//   let uniqueLength = nums.length;
-//   return nums;
+//   let k = nums.length;
+//   return k;
 // };
+// console.log(removeDuplicates([1, 1, 2]), 2);
+// console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]), 5);
 
-// USING FILTER AND FIND?
-// Does filter edit the array IN PLACE? I don't think so
-// nums[0].filter(el => el.find(item => item === el); // This filter method is not going well
+// SOLUTION USING SPLICE - QUADRATIC O(n ^2) because the array is re-indexed on each splice()
+function removeDuplicates(nums) {
+  // Handle edge case of an empty array
+  if (nums.length === 0) {
+    return 0;
+  }
 
-// LEET CODE ANSWER - RUNTIME 129 ms
-// var removeDuplicates = function(nums) {
-//     for (i = 0; i < nums.length; i++) {
-//         //Next number is identical to current one
-//         if (nums[i] == nums[i+1]) {
-//             nums.splice(i, 1);
-//             i--;
-//         }
-//     }
-// };
+  let uniqueCount = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1);
+      i--; // Decrement the index to recheck the current position in the next iteration
+    } else {
+      uniqueCount++;
+    }
+  }
+
+  // Include the last element as unique
+  uniqueCount++;
+
+  return uniqueCount;
+}
 
 console.log(removeDuplicates([1, 1, 2]), 2);
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]), 5);
+
+// SOLUTION USING TWO "POINTERS" - Linear time complexity O(n) because we overwrite the existing values in the array - no re-indexing
+// function removeDuplicates(nums) {
+//   if (nums.length === 0) {
+//     return 0;
+//   }
+//   // Use two pointers: one to track the last unique element position and the other for iteration
+//   let uniqueIndex = 0;
+
+//   // Iterate through the array starting from the second element
+//   for (let i = 1; i < nums.length; i++) {
+//     // If the current element is different from the previous unique element, update the uniqueIndex
+//     if (nums[i] !== nums[uniqueIndex]) {
+//       uniqueIndex++;
+//       nums[uniqueIndex] = nums[i];
+//     }
+//   }
+//   // The number of unique elements is the uniqueIndex + 1
+//   console.log(nums);
+//   return uniqueIndex + 1;
+// }
+
+// // Example usage:
+
+// console.log(removeDuplicates([1, 1, 2]), 2);
+// console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]), 5);
