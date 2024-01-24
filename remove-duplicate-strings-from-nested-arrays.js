b3905ecdb613399c52a7afdcc2d85f30154abbfb
Eric Carlson <eric.carlson2972@gmail.com>
2024-01-24 05:52:43 -0500

    add solution to existing ones

--- remove-duplicate-strings-from-nested-arrays.js
+++ remove-duplicate-strings-from-nested-arrays.js
@@ -1,6 +1,6 @@
-//Remove any duplicated strings within each of the nested arrays.
+// Remove any duplicated strings within each of the nested arrays.
 
-//Example
+// Example
 // Input: Some of the nested arrays have duplicate strings
 // [
 //   ["apple", "banana", "pear", "orange"],
@@ -15,11 +15,11 @@
 // ]
 
 // SOLUTION USING MAP() AND A SET
-// function uniqueArrays(arrayOfArrays) {
-//   return arrayOfArrays.map(subarray => [...new Set(subarray)]);
-// }
+function uniqueArrays(arrayOfArrays) {
+  return arrayOfArrays.map(subarray => [...new Set(subarray)]);
+}
 
-// Nested loops O(n * m) - n = nested arrays, m = items in each array
+//Using a map to track repeated occurrences within each array - O(n * m)
 // function uniqueArrays(arrayOfArrays) {
 //   const uniques = arrayOfArrays.map(subarray => {
 //     const occurrences = {};
@@ -37,6 +37,61 @@
 //   return uniques;
 // }
 
+// USING MAP WITH A FOR() LOOP NESTED INSIDE OF IT
+// function uniqueArrays(arrayOfArrays) {
+//   const uniques = arrayOfArrays.map(subarray => {
+//     const occurrences = {};
+//     const uniqueSubarray = [];
+
+//     //Loop through each subarray and push only the first occurrence to the new subarray
+//     for (let i = 0; i < subarray.length; i++) {
+//       if (!occurrences[subarray[i]]) {
+//         occurrences[subarray[i]] = true;
+//         uniqueSubarray.push(subarray[i]);
+//       }
+//     }
+//     return uniqueSubarray;
+//   });
+//   return uniques;
+// }
+
+// USING TWO NESTED FOR() LOOPS INSTEAD OF USING MAP FOR THE
+// function uniqueArrays(arrayOfArrays) {
+//   const uniques = [];
+//   for (let j = 0; j < arrayOfArrays.length; j++) {
+//     // const uniques = arrayOfArrays.map(subarray => {
+//     const occurrences = {};
+//     const uniqueSubarray = [];
+
+//     //Loop through each subarray and push only the first occurrence to the new subarray
+//     for (let i = 0; i < arrayOfArrays[j].length; i++) {
+//       if (!occurrences[arrayOfArrays[j][i]]) {
+//         occurrences[arrayOfArrays[j][i]] = true;
+//         uniqueSubarray.push(arrayOfArrays[j][i]);
+//       }
+//     }
+//     // return uniqueSubarray;
+//     // });
+//     uniques.push(uniqueSubarray);
+//   }
+//   return uniques;
+// }
+
+// Using filter() with the occurance map instead of a manual loop
+// function uniqueArrays(arrayOfArrays) {
+//   return arrayOfArrays.map(subarray => {
+//     const seen = {};
+//     return subarray.filter(item => {
+//       if (!seen[item]) {
+//         seen[item] = true;
+//         return true;
+//       }
+//       return false;
+//     });
+//   });
+// }
+
+// You COULD just use includes() to test the array itself, but the time complexity would not be as good as the object lookup !!
 // function uniqueArrays(arrayOfArrays) {
 //   const uniques = arrayOfArrays.map(subarray => {
 //     const occurrences = {};
@@ -51,17 +106,6 @@
 //   });
 
 //   return uniques;
-// }
-
-function uniqueArrays(arrayOfArrays) {
-  const occurrences = {};
-  const uniqueSubarray = [];
-
-  arrayOfArrays;
-}
-
-// function uniqueArrays(arrayOfArrays) {
-//   return arrayOfArrays.map(subarray => [...new Set(subarray)]);
 // }
 
 console.log(
