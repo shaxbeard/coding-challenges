d7a1861e1e9834900b7555bcfe82a66bb874be8e
Eric Carlson <eric.carlson2972@gmail.com>
2024-01-25 05:51:54 -0500

    add new solutio

--- remove-anchor-from-url-7str.js
+++ remove-anchor-from-url-7str.js
@@ -10,8 +10,35 @@
 //pseudocode
 //methdods
 
-const removeAnchor = url => (url.indexOf("#") === -1 ? url : url.split("#")[0]);
+// Method #1 - manual transmission - C syntax only with a for() loop
+// function removeUrlAnchor(url) {
+//   let string = "";
+//   debugger;
+//   for (let i = 0; i < url.length; i++) {
+//     //Once you hit the #, break out of the for() loop and return the new string
+//     if (url[i] === "#") {
+//       break;
+//     }
+//     string += url[i];
+//   }
+//   return string;
+// }
 
-console.log(removeAnchor("www.codewars.com#about"), "www.codewars.com");
-console.log(removeAnchor("www.codewars.com#contactus"), "www.codewars.com");
-console.log(removeAnchor("www.codewars.com?page=1"), "www.codewars.com?page=1");
+// Method #2 - using built-in string method(s)
+// function removeUrlAnchor(url) {
+//   // Split the string into an array of two string using the # as the "splitter"
+//   // Return the string at the first index of the array - the part BEFORE the #
+//   return url.split("#")[0];
+// }
+
+function removeUrlAnchor(url) {
+  var index = url.indexOf("#");
+  return index == -1 ? url : url.slice(0, index);
+}
+
+console.log(removeUrlAnchor("www.codewars.com#about"), "www.codewars.com");
+console.log(removeUrlAnchor("www.codewars.com#contactus"), "www.codewars.com");
+console.log(
+  removeUrlAnchor("www.codewars.com?page=1"),
+  "www.codewars.com?page=1"
+);
