901f152a42b505b671c537c1bee17a9ddb770f46
Eric Carlson <eric.carlson2972@gmail.com>
2024-02-01 07:18:40 -0500

    complete the solution that I have worked on before

--- max-character.js
+++ max-character.js
@@ -1,21 +1,12 @@
 // Given a string of characters, return the character that appears the most often.
 
-// describe("Max Character", () => {
-//  it("Should return max character", () => {
-//   assert.equal(max("Hello World!"), "l");
-//  });
-// });
-
-// No String or Array Methods (well brute force it first, but then no methods)!
-
-// MAKE A CHAR MAP THEN LOOP OVER THE COMPLETED MAP TO FIND MAX VALUE
+// MAKE A CHAR MAP THEN LOOP OVER THE COMPLETED MAP TO FIND MAX VALUE - O(n) + O(n) = O(2n) = O(n)
+// Time complexity is O(n) - the second loop over the chars is constant time and insignificant overall
 // function maxCharacter(str) {
-//   // char map
 //   const map = {};
 //   for (const char of str) {
 //     !map[char] ? (map[char] = 1) : map[char]++;
 //   }
-//   // find the highest number in the char map
 //   let maxCount = 0;
 //   let maxChar = "";
 //   for (const char in map) {
@@ -27,20 +18,22 @@
 //   return maxChar;
 // }
 
-// MAKE A CHAR MAP THEN SORT THE MAP AND SELECT THE FIRST ELEMENT
+// console.log(maxCharacter("abracadabra"), "a");
+
+// // MAKE A CHAR MAP THEN SORT THE MAP AND SELECT THE FIRST ELEMENT - O(n) + O(n log n) = O(n log n)
+// // Time complexity if O(n log n) ???
 function maxCharacter(str) {
-  function charMap(str) {
-    const map = {};
-    for (const char of str) {
-      !map[char] ? (map[char] = 1) : map[char]++;
-    }
-    return map;
-  }
   const map = charMap(str);
-  console.log("This is the actual char map: ", map);
-  console.log("This is the char map with Object.values: ", Object.values(map));
-  console.log("This is the char map with Object.entrie: ", Object.entries(map));
-  // return Object.entries(map).sort(([, a], [, b]) => b - a)[0][0];
+  const sortedChars = Object.keys(map).sort((a, b) => map[b] - map[a]);
+  return sortedChars[0];
 }
 console.log(maxCharacter("Hello World!"), "l");
-// console.log(maxCharacter("abracadabra"), "a");
+console.log(maxCharacter("abracadabra"), "a");
+
+// I will put the charMap function at the bottom to keep it out of the way
+function charMap(str) {
+  return str.split("").reduce((map, char) => {
+    !map[char] ? (map[char] = 1) : map[char]++;
+    return map;
+  }, {});
+}
