20789f120ee9b070458ee23a700b25fefc332dd0
Eric Carlson <eric.carlson2972@gmail.com>
2024-04-25 06:06:30 -0400

    initial commit

--- test.js
+++ test.js
@@ -1,30 +1,7 @@
-
- // Loop over each letter in str
-  // Return an array with the entire word "hello" on each iteration
-      // BUT one letter of each of these words will be capitalized
-      // On the first iteration , capitalize the first letter
-      // On the second iteration, capitalize the second letter
-      // ...
-
-//  0     1    2    3    4
-// ["h", "e", "l", "l", "o"]
-//             ^
-// ["H", "e", "l", "l", "o"]
-// ["h", "E", "l", "l", "o"]
-// ["h", "e", "L", "l", "o"]
-// ...
-
-function wave(str) {
-  // Convert the string to an array of letters
-  // Loop over the array of letters
-  // Return the letters before the index 
-      // + the current letter capitalized
-      // + the letters after the index
- 
-  return str.split("").map((char, index) => str.slice(0, index) + char.toUpperCase() + str.slice(index + 1)).filter(x => x != str);
-}
-
-
-console.log(wave("hello"), ["Hello", "hEllo", "heLlo", "helLo", "hellO"])
-console.log(wave("du h"), ["Duh", "dUh", "duH"])
-console.log(wave(""))
+const str = "hello";
+console.log("CharAt: ", str.charAt(2));
+console.log("CharCodeAt: ", str.charCodeAt(2));
+console.log("CodePointAt: ", str.codePointAt(2));
+console.log("Concat: ", str.concat(" ", "world"));
+console.log("EndsWith: ", str.endsWith("llo"));
+console.log("Includes: ", str.includes("el"));
---
+++ uefa-euro-2016-8str.js
@@ -0,0 +1,18 @@
+
+// Finish the uefaEuro2016() function so it return string just like in the examples below:
+
+// uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"
+// uefaEuro2016(['Belgium', 'Italy'],[0, 2]) // "At match Belgium - Italy, Italy won!"
+// uefaEuro2016(['Portugal', 'Iceland'],[1, 1]) // "At match Portugal - Iceland, teams played draw."
+
+function uefaEuro2016(teams, scores) {
+    const [team1, team2] = teams
+    const [score1, score2] = scores
+    const winner = score1 > score2 ? team1 : team2
+
+    return score1 === score2
+        ? `At match ${team1} - ${team2}, teams played draw.`
+        : `At match ${team1} - ${team2}, ${winner} won!`
+}
+
+console.log(uefaEuro2016(["Germany", "Ukraine"], [2, 0]))
