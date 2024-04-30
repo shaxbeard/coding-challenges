0ddc54817dd1ba9b98d65f5b0b87d9218a23f492
Eric Carlson <eric.carlson2972@gmail.com>
2024-04-30 06:07:03 -0400

    initial commit

---
+++ find-the-first-nonconsecutive-number-8arr.js
@@ -0,0 +1,12 @@
+//  0 1 2 3 4 5  # indexes
+// [1,2,3,4,6,7] # array
+
+
+function firstNonConsecutive (arr) {
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] !== arr[i - 1] + 1) return arr[i]
+    }
+    return null;
+}
+        
+console.log(firstNonConsecutive([1,2,3,4,6,7]), 6)
