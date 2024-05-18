99b10625cbdfa8b6f25eb56bf33dae5913e72889
Eric Carlson <eric.carlson2972@gmail.com>
2024-05-18 05:15:32 -0400

    initial commit

---
+++ find-the-first-python-developer-7arr.js
@@ -0,0 +1,36 @@
+// You will be given an array of objects (associative arrays in PHP) 
+// representing data about developers who have signed up to attend the 
+// next coding meetup that you are organising. 
+// The list is ordered according to who signed up first.
+
+// Your task is to return one of the following strings:
+// < firstName here >, < country here > of the first Python developer who has signed up; 
+// or "There will be no Python developers" if no Python developer has signed up.
+
+// For example, given the following input array:
+// var list1 = [
+//   { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+//   { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+// ];
+
+var list1 = [
+    { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+    { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+    { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+  ];
+
+  var list2 = [
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+    { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' },
+  ];
+
+function getFirstPython(list) {
+    const pythonDev = list.find(dev => dev.language === "Python");
+    return pythonDev ? `${pythonDev.firstName}, ${pythonDev.country}` : "There will be no Python developers";
+}
+
+console.log(getFirstPython(list1), 'Victoria, Puerto Rico')
+console.log(getFirstPython(list2), 'There will be no Python developers')
+
+
