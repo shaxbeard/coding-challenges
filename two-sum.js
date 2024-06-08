88a248f5c282dfdb17a8b0203ed90a3e41df6b92
Eric Carlson <eric.carlson2972@gmail.com>
2024-06-07 21:27:00 -0400

    new solution added

--- two-sum.js
+++ two-sum.js
@@ -1,42 +1,38 @@
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// Given an array of integers nums and an integer target, 
+// return indices of the two numbers such that they add up to target.
 
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-// You can return the answer in any order.
-
-// Inputs are
-// #1 an array of integers # NOT NECESSARILY SORTED IN THIS CASE
-// #2 an integer target
-// Return the indices of the two ints whose sum = target
 
 // Example 1:
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].]
-// What is the state of our loop and map when the solution is reaches?
-// map = { 2: 0}
-// loop - nums[i] = 7
+
 
 // Example 2:
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
 
-// Example 3:
-// Input: nums = [3,3], target = 6
-// Output: [0,1]
+// Let's change the array to [8, 2, 5, 7]
 
-// Input is an array of nums AND a "target" number
-// Return the INDICES of two nums from the array that add up to target
-// Examples are explained very well for us above
-//
+//  0   1   2    3
+// [8,  2,  5,   7]  # target = 9
+// 9-8 9-2 9-5, 9-7
+// no, no, no,  yes  // is the diff already in the hashmap?
+
+
+// HashMap
+// val: index
+//  8 :  0
+//  2 :  1
+//  5 :  2
+//  7 :  3
+
+
 
 // BRUTE FORCE (nested for loops) - O(n^2) quadratic
 // function twoSum(nums, target) {
-//   // Outer loop for i
 //   for (let i = 0; i < nums.length; i++) {
-//     // Inner loop for j
 //     for (let j = i + 1; j < nums.length; j++) {
-//       // If nums[i] + nums[j] = target
 //       if (nums[i] + nums[j] === target) {
 //         return [i, j];
 //       }
@@ -54,15 +50,14 @@
 
 // Linear time complexity O(n) - You calculate the sum AS YOU ARE BUILDING THE MAP
 function twoSum(nums, target) {
-  // Create an empty object to store the numbers in the input array as keys and their indices as values.
+  // Create an empty object to store the nums as keys and THEIR INDEXES as values.
   const numMap = {};
-
-  // Loop through each number in the array  - we are NOT looping through the numMap
   for (let i = 0; i < nums.length; i++) {
     // Check if the difference between the target value and the current ARRAY number is already a KEY in the numMap object.
-    if (target - nums[i] in numMap) {
-      // If it is, return an array containing the index of the current number and the index of the number in numMap that adds up to the target value.
-      return [numMap[target - nums[i]], i];
+    let diff = target - nums[i];
+    if (diff in numMap) {
+      // If it is, return an array with the index of the current number and the index of the number in numMap that adds up to the target value.
+      return [numMap[diff], i];
     }
     // If the difference is not in the numMap object, add the current number and its index to the object.
     numMap[nums[i]] = i;
