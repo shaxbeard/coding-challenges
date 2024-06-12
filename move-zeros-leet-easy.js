f92068a1bac0a10392bdeda3a66c820d8efb228b
Eric Carlson <eric.carlson2972@gmail.com>
2024-06-12 06:19:07 -0400

    add new solution

--- move-zeros-leet-easy.js
+++ move-zeros-leet-easy.js
@@ -1,5 +1,4 @@
-// Given an integer array nums, 
-// move all 0's to the end of it 
+// Given an integer array nums, move all 0's to the end of it 
 // while maintaining the relative order of the non-zero elements.
 
 // Note that you must do this in-place without making a copy of the array.
@@ -8,41 +7,46 @@
 
 
 // Example 1:
-// Input: nums = [0,1,0,3,12]
-// Output: [1,3,12,0,0]
-
-// 0 1 2 3 4 
-//[1,0,3,12,0]
-// ^
+// Input: nums = [8,0,1,0,3,12]
+// Output: [8,1,3,12,0,0]
 
 
-// function moveZeros(nums) {
-//     for(var i = nums.length;i--;)  { // where is the condition?
-//         if(nums[i]===0){
-//             nums.splice(i,1)
-//             nums.push(0);
-//         }
-//     }
-//     return nums;
-// }
+// 0 1 2 3 4 5 
+//[8,1,3,12,0,0]   # nums array
+//       i
+//            j                         # Loop until j reaches the end
 
-// function moveZeros(nums) {
-//     let j = 0  // nonzero count
-//     for (let i = 0; i < nums.length; i++) {
-//       if (nums[i] !== 0) {
-//         // swap elements - what kind of a swap mechanism is this?
-//         [nums[j], nums[i]] = [nums[i], nums[j]]
-//         j++
-//       }
-//     }
-//     return nums;
-// }
+// Key mechanisms for using two pointeres
+// 1. If i !== the target, then increment BOTH pointers  # Do you really want this in EVERY condition?
+// 
+
+// Loop using 2 pointers - so use while() with indexes starting at i = 0 and j = i + 1
+function moveZeros(nums) {
+  let i = 0;
+  let j = i + 1;
+  // Keep looping until the second pointer (j) is larger than nums.length - 1
+  while (j <= nums.length - 1) {
+  // Keep incrementing BOTH indexes until num[i] equals the target (zero in this case)
+    if (nums[i] !== 0) {
+      i++;
+      j++;
+      // ELSE (so nums[i] DOES EQUAL ZERO)  
+    } else {
+        // Swap nums[i] with nums[j] and increment both i and j  # Caveat here 
+        ///////  Increment i ONLY IF nums[j] DOES NOT ALSO EQUAL ZERO 
+        if (nums[j] !== 0) {
+          [nums[i], nums[j]] = [nums[j], nums[i]]
+          i++
+        }
+        // ALWAYS INCRMENT J - EVEN IF J === 0
+        j++
+      }
+    }
+}
 
 
-// 0 1 2 3 4 
-//[1,3,12,0,0]
-//        i
-//             ** j = 5         // Loop until j reaches the end
+
+////  Increment j no matter what (here in the ELSE condition where nums[i] !== 0)
 
 
 // METHOD 1 - TOP SOLUTION ON LEETCODE
