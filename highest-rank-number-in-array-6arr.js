ff292d57dded8523b6328248954bde629abe2be3
Eric Carlson <eric.carlson2972@gmail.com>
2024-06-22 06:01:17 -0400

    added new solution

--- highest-rank-number-in-array-6arr.js
+++ highest-rank-number-in-array-6arr.js
@@ -6,19 +6,29 @@
 // [12, 10, 8, 12, 7, 6, 4, 10, 12, 10]          -->  12
 // [12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]  -->   3
 
-//freqMap: {12: 3, 10: 2, 8: 1, 7: 1, 6: 1, 4: 1}
-// [12, 10, 8, 12, 7, 6, 4, 10, 12]
-//
+//[10, 12, 8, 12, 7, 6, 4, 10, 12, 10]
 
-// {12: 3, 10: 2, 8: 1, 7: 1, 6: 1, 4: 1} // Only 1 num has highest frequency
-// {12: 3, 10: 3, 8: 1, 7: 1, 6: 1, 4: 1} // MORE THAN 1 num has highest frequency
+// {'12': 3, '10': 3, '8': 1, '7': 1, '6': 1, '4': 1} // MORE THAN 1 num has highest frequency
 //  maxFreq = 3
-
-// {12: 3} // only 1 maxFreq
-// { 12: 3, 10: 3, 99: 3 }  // More than 1 maxFreq
+// ['12','10','8','7','6','4'] # Just the keys in an array
+// ['12','10']   # Just the keys with the value of maxFreq
+// 12 - It looks like Math.max(...['12', '10']) changes '12' from a string to a num (12)??     
 
 // Method #1 - Make a frequency map, then extract the number(s) with the max frequency
 function highestRank(arr) {
+  const freqMap = frequencyMap(arr);
+  const maxFreq = maxFrequency(freqMap); // What is the value of the max frequency of any num?
+  
+  // Extract just the keys from the map and filter to keep just the keys with the max frequency VALUE
+  const filteredKeys = Object.keys(freqMap).filter(
+    key => freqMap[key] === maxFreq
+  );
+  // If 2 nums have the same frequency, select the higher num
+  // return Math.max(...filteredKeys.map(Number)); 
+  return Math.max(...filteredKeys); 
+}
+
+function frequencyMap(arr) {
   const freqMap = {};
   for (let num of arr) {
     if (!freqMap[num]) {
@@ -27,26 +37,25 @@ function highestRank(arr) {
       freqMap[num]++;
     }
   }
+  return freqMap;
+}
 
+function maxFrequency(freqMap) {
   let maxFreq = 0;
   for (let num in freqMap) {
     if (freqMap[num] > maxFreq) {
       maxFreq = freqMap[num];
     }
   }
-
-  const filteredKeys = Object.keys(freqMap).filter(
-    key => freqMap[key] === maxFreq
-  );
-  return Math.max(...filteredKeys.map(Number));
+  return maxFreq;
 }
 
 // Method #2 - Clever use of sort() along with filter()
-function highestRank(arr) {
-  var getNum = num => arr.filter(n => n === num).length;
-  return arr.sort((a, b) => getNum(b) - getNum(a) || b - a)[0];
-}
+// function highestRank(arr) {
+//   var getNum = num => arr.filter(n => n === num).length;
+//   return arr.sort((a, b) => getNum(b) - getNum(a) || b - a)[0];
+// }
 
-console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12]), 12);
-console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12, 10]), 12); // Both 10 and 12 appear two times
-console.log(highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]), 3);
+// console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12]), 12);
+console.log(highestRank([10, 12, 8, 12, 7, 6, 4, 10, 12, 10]), 12); // Both 10 and 12 appear two times
+// console.log(highestRank([12, 10, 8, 8, 3, 3, 3, 3, 2, 4, 10, 12, 10]), 3);
