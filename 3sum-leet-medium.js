fe4def254d3659512a86349d8319266f5d5fc6a7
Eric Carlson <eric.carlson2972@gmail.com>
2024-07-07 05:36:59 -0400

    new solution added

--- 3sum-leet-medium.js
+++ 3sum-leet-medium.js
@@ -13,19 +13,40 @@
 // The distinct triplets are [-1,0,1] and [-1,-1,2].
 // Notice that the order of the output and the order of the triplets does not matter.
 
-
 //[-3,3,4,-3,1,2]
-
-
 
 // _ + _ + _ = 0
 // a   b   c  
 
-
-
-
 function threeSum(nums) {
-    return;
+    const res = [];
+    nums.sort((a, b) => a - b);
+
+    // nums[i] will point to the "a" - the first of the 3sum nums
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) break; //This is not in the Python solution, and the function passes leetcode without it
+        if (i > 0 && nums[i] === nums[i - 1]) continue; //Ignore duplicates
+        
+        // Now, the rest is mostly the Two Pointer solution to Two Sum II
+        let l = i + 1; // i + 1 because we start 1 to the right of number "a"
+        let r = nums.length - 1;
+        while (l < r) {
+            const sum = nums[i] + nums[l] + nums[r];
+            if (sum > 0) { // We are looking for a sum of zero this time
+                r--;
+            } else if (sum < 0) {
+                l++;
+            } else { // If the sum actually equals zero
+                res.push([nums[i], nums[l], nums[r]]);
+                l++;
+                r--;
+                while (l < r && nums[l] === nums[l - 1]) { // Avoid duplicates again
+                    l++; //Only need to increment 1 pointer here
+                }
+            }
+        }
+    }
+    return res;
 }
 
 console.log(threeSum([-1,0,1,2,-1,-4]), [[-1,-1,2],[-1,0,1]])
