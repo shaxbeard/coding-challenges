133bb9ac021b7041a2850b3a5624fdcf925a6082
Eric Carlson <eric.carlson2972@gmail.com>
2024-07-09 06:43:38 -0400

    new solution added

--- valid-anagram-leet-easy.js
+++ valid-anagram-leet-easy.js
@@ -8,14 +8,31 @@
 
 // METHOD #1 - sort each word and compare the sorted strings O(N log N) * 2
 
-
-
 // METHOD #2 - create two frequency maps ("hash table histograms") and compare the values
 // {s: 1, t: 2, a: 1, e: 1}  # for "state"
 // {s: 1, t: 2, a: 1, e: 1}  # for "taste"
 
+// s = state, t = stated
+//{s:1, t:2, a:1, e:1} for "state"
+//                 ^
+//{s:1, t:2, a:1, e:1, d:1} for "stated"
+
 function anagram(s, t) {
-    return;
+    if (s.length !== t.length) return false;
+    const map1 = freqMap(s);
+    const map2 = freqMap(t);
+    for (let key in map1) {
+        if (map1[key] !== map2[key]) return false;
+    }
+    return true;
+}
+
+function freqMap(str) {
+    const map = {};
+    for (let char of str) {
+        !map[char] ? map[char] = 1 : map[char]++;
+    }
+    return map;
 }
 
 console.log(anagram("silent", "listen"), true)
