4de98641fa887bea24b5cd7a174c9ba43313442a
Eric Carlson <eric.carlson2972@gmail.com>
2024-08-06 06:04:59 -0400

    initial commits

---
+++ linked-lists-double-linked-lists-ztm.js
@@ -0,0 +1,97 @@
+// How would you render this linked list structure with a simple JS object?
+// 10 -> 5 -> 16
+
+// Using a simple object with nodes as nested items
+// let myLinkedList = {
+//     head: {
+//         value: 10,
+//         next: {
+//             value: 5,
+//             next: {
+//                 value: 16,
+//                 next: null
+//             }
+//         }
+//     }
+// }
+
+// CREATE A CLASS THAT GENERATES OBJECTS LIKE ABOVE
+class DoublyLinkedList {
+    constructor(value) {
+        this.head = {
+            value: value,
+            next: null,
+            prev: null
+        }
+    this.tail = this.head;
+    this.length = 1;
+    }
+    append(value) {
+        const newNode = {
+            value: value,
+            next: null,
+            prev: null
+        };
+        newNode.prev = this.tail; // Set the value of the prev pointer to the node that WAS the tail
+        this.tail.next = newNode; // Make a pointer from the old tail to the new node
+        this.tail = newNode; // create the new node itself at the tail
+        this.length++
+        return this;
+    }
+    prepend(value) {
+        const newNode = {
+            value: value,
+            next: null
+        };
+        newNode.next = this.head // Make a next pointer from the new node to the old head
+        this.head.prev = newNode; // Make a prev pointer from the old head to the new node
+        this.head = newNode; // Create the new node itself at the head
+        this.length++
+        return this;
+    }
+    printList() {
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+    }
+    insert(index, value) {
+        //check params
+        if (index >= this.length) {
+            return this.append(value);
+        }
+        const newNode = {
+            value: value,
+            next: null,
+            prev: null
+        };
+        const leader = this.traverseToIndex(index-1);
+        const follower = leader.next;
+        leader.next = newNode;
+        newNode.prev = leader;
+        newNode.next = follower;
+        follower.prev = newNode;
+    }
+    // remove()  // COME BACK TO THIS LATER
+
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
+}
+
+const myLinkedList = new DoublyLinkedList(10);
+myLinkedList.append(5)
+myLinkedList.append(16)
+myLinkedList.prepend(1)
+myLinkedList.printList();
+console.log(myLinkedList)
+
+
