b9ca52ef31107e0d91899ba9b7c8e96d80a02527
Eric Carlson <eric.carlson2972@gmail.com>
2024-08-15 06:23:15 -0400

    initial commit

---
+++ find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,14 @@
+def strStr(haystack, needle):
+    if needle == "":
+        return 0
+    
+    for i in range(len(haystack) + 1 - len(needle)):
+        for j in range(len(needle)):
+            if haystack[i + j] != needle[j]:
+                break
+            if j == len(needle) - 1:
+                return i
+    return -1
+
+
+ 
