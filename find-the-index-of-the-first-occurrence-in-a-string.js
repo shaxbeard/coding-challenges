ec9b7759ee7e82e29ac5875ac33789baa7cd0a1c
Eric Carlson <eric.carlson2972@gmail.com>
2024-08-15 06:23:34 -0400

    improve the solution

--- find-the-index-of-the-first-occurrence-in-a-string.js
+++ find-the-index-of-the-first-occurrence-in-a-string.js
@@ -2,7 +2,6 @@
 // return the index of the first occurrence of needle in haystack, 
 // or -1 if needle is not part of haystack.
 
- 
 // Example 1:
 // Input: haystack = "sadbutsad", needle = "sad"
 // Output: 0
@@ -14,9 +13,39 @@
 // Output: -1
 // Explanation: "leeto" did not occur in "leetcode", so we return -1.
 
-function strstr(haystack, needle) {
-    return;
-};
+// Input: two strings - any funny business? empty string, etc.? no
+// Return the index of 1st occurrence of str2 in str1
 
+// wordIndex =
+// DEMO
+//  012345678901      012
+// "zzsazbusadtsad", "sad"
+//  i           
+//                    j
+
+// METHOD 1 - Naive solution
+// I didn't know you could use indexOf() with a word !!!!
+// function strstr(haystack, needle) {
+//   return haystack.indexOf(needle);
+// }
+
+// METHOD 2 - BRUTE FORCE WITH NESTED FOR LOOPS ,Time = O(N * M)
+// This is also the wrong answer, right? - Neetcode says this should be your solution
+// Neetcode says that the KMP algorithm is O(n + m) but it is too complicated to learn for an interview
+
+const strstr = (haystack, needle) => {    
+    for (let i = 0; i < haystack.length - needle.length + 1; i++) {    // start looping through haystack until the remaining substring is shorter than needle
+    //   if (haystack[i] === needle[0]) {             // WE DON'T NEED THIS CHECK - THE TEST BELOW ALREADY DOES THIS ! 
+        for (let j = 0; j < needle.length; j++) {    // start looping through both needle and haystack
+          if (needle[j] !== haystack[i + j]) break;  // when the characters don't match, break out of the loop and return to looping through haystack                                
+          if (j === needle.length - 1) return i;     // otherwise, if all of the characters matched, // return the index of the first character of haystack with which we started the loop
+        }
+    }
+    return -1;                                        // return -1 if there wasn't a match
+  };
+
+
+
+console.log(strstr("zzsazbusadtsad", "sad"), 7)
 console.log(strstr("sadbutsad", "sad"), 0)
 console.log(strstr("leetcode", "leeto"), -1)
