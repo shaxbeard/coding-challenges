4fde95bc6687e198446b1b143350a44943b40bf7
Eric Carlson <eric.carlson2972@gmail.com>
2024-08-30 06:22:40 -0400

    initial commit

---
+++ validate-binary-search-tree.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        def valid(node, left, right):
+            if not node:
+                return True # an empty BST is technicall valid
+            if not (node.val < right and node.val > left):
+                return False
+            
+            # node.val is the num that node.right must be less than (in the demo)?
+            # does this also go for the left side? (he didn't explain this for the left)
+            return (valid(node.left, left, node.val) and valid(node.right, node.val, right))
+        return valid(root, float("-inf"), float("inf")) # start with -Infinity and +Infinity
+
