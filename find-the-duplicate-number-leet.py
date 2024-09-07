03f985f8ddda7e0c8d9682d773550b0d39a58ad4
Eric Carlson <eric.carlson2972@gmail.com>
2024-09-07 06:35:39 -0400

    add comments that explain the solution in detail

--- find-the-duplicate-number-leet.py
+++ find-the-duplicate-number-leet.py
@@ -1,7 +1,15 @@
+# 287. Find the Duplicate Number
+
+# Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+# There is only one repeated number in nums, return this repeated number.
+# You must solve the problem without modifying the array nums and uses only constant extra space.
+
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         slow, fast = 0, 0 # Start both pointers at 0 - before the cycle
-        while True: # This is the find the intersection point
+        # Phase 1 - Find the intersection point
+        while True: 
             # NB - we are treating an array as if it were a linked list
             slow = nums[slow] # This points to the next index
             fast = nums[nums[fast]] # This points to the next next index
