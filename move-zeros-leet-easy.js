037dbf7fde5e040da9c48978475fbf8d93e5675e
Eric Carlson <eric.carlson2972@gmail.com>
2024-09-23 05:51:41 -0400

    add new solution

--- move-zeros-leet-easy.js
+++ move-zeros-leet-easy.js
@@ -3,29 +3,31 @@
 
 // Note that you must do this in-place without making a copy of the array.
 
-// Follow up: Could you minimize the total number of operations done?
-
 
 // Example 1:
 // Input: nums = [8,0,1,0,3,12]
 // Output: [8,1,3,12,0,0]
 
 
-// 0 1 2 3 4 5 
-//[8,1,3,12,0,0]   # nums array
-//       i
-//            j                         # Loop until j reaches the end
+// DEMO
+//[8,4,3,2,1,2,3,12,0,0]
+//               i
+//                   j                         # Loop until j reaches the end
 
 // Key mechanisms for using two pointeres
-// 1. If i !== the target, then increment BOTH pointers  # Do you really want this in EVERY condition?
-// 
+// 1. If i !== the target, then increment BOTH pointers  
+// I like the variation on the mechanism of two pointers here. The trailing pointer is the primary mover,
+// so you increment BOTH pointers while i !== 0. In this way, the mechanism is almost like the simple
+// "cur" and "next" pointers in the Roman Numerals problem. BUT, if j === 0, then you need to move it ahead
+// on its own - so now it "decouples" the cur/next set (like the Star Trek TNG Enterprise when it
+// separates the saucer section from the rest of the ship).
 
 // Loop using 2 pointers - so use while() with indexes starting at i = 0 and j = i + 1
 function moveZeros(nums) {
   let i = 0;
   let j = i + 1;
   // Keep looping until the second pointer (j) is larger than nums.length - 1
-  while (j <= nums.length - 1) {
+  while (j < nums.length) {
   // Keep incrementing BOTH indexes until num[i] equals the target (zero in this case)
     if (nums[i] !== 0) {
       i++;
@@ -42,44 +44,7 @@ function moveZeros(nums) {
         j++
       }
     }
-}
-
-
-
-////  Increment j no matter what (here in the ELSE condition where nums[i] !== 0)
-
-
-// METHOD 1 - TOP SOLUTION ON LEETCODE
-// let moveZeros = function (nums) {
-//     let i = 0;
-//     let j = i + 1;
-
-//     while (j <= nums.length - 1) { // j = 5 AND nums.length = 5
-//         if (nums[i] !== 0) { // If both nums !== 0, increment both indexes
-//             i++;
-//             j++;
-//         } else { // If just the HIGH num !== 0, swap the zero and the high num
-//             if (nums[j] !== 0) {
-//                 [nums[i], nums[j]] = [nums[j], nums[i]];
-//                 i++;
-//             }
-//             j++;
-//         }
-//     }
-//     return nums;
-// };
-
-// METHOD 2 - MORE CONCISE - SEEMS TO WORK FOR THIS PROBLEM IN PARTICULAR
-function moveZeros(nums) {
-    var j = 0;
-    for (var i = 0; i < nums.length; i++) {
-      if (nums[i] !== 0) {
-        nums[j] = nums[i];
-        nums[i] = j === i ? nums[i] : 0;
-        j++;
-      }
-    }
     return nums;
-  }
+}
 
 console.log(moveZeros([0,1,0,3,12]),[1,3,12,0,0])
