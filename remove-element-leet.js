72bf0dd5394522458fd995092c22c7ff821f9433
Eric Carlson <eric.carlson2972@gmail.com>
2024-10-13 22:21:16 -0400

    initial commit

--- remove-element-leet.js
+++ remove-element-leet.js
@@ -24,9 +24,54 @@
 // Note that the five elements can be returned in any order.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
 
+// val = 3
+// [3,2,2,3] - arr of int
+// [_,2,2,_] -> remove the 3s IN PLACE - no new array
+// [2,2,_,_]
+
+// SANDBOX
+
+// FOR THE PURPOSE OF DEMONSTRATION, it would be clearer if I actually SWAPPED the values - so the swapped nums are together at the end
+// Then refactor the swap to optimize the swap
+
+// FULL SWAP DEMO
+// val = 2
+//  0,1,2,3,4,5,6,7   - indexes
+// [0,1,4,0,3,2,2,2]  - the final array (the final 3 nums are irrelevant anyway)
+//          l        
+//          r         - the length of the array items OTHER THAN VAL === the final position of l + 1 (or r + 1)
+//                    - we COULD edit the loop so it increments one more time, but that is less clear
+
+// The solution if I optimize the swap to nums[l] = nums[r] only
+// val = 2
+//  0,1,2,3,4,5,6,7   - indexes
+// [0,1,4,0,3,0,4,2]  - the final array (the final 3 nums are irrelevant anyway)
+//          l        
+//          r
+
+
+
+
 function removeElement(nums, val) {
+    let l = 0;
+    let r = nums.length - 1;
     
+    while (l < r) {
+        if (nums[l] === val) {
+            // nums[l] = nums[r]; // refactor - no need for full swap
+            [nums[l], nums[r]] = [nums[r], nums[l]]; // start with a full swap to see results better
+            r--;
+            console.log(nums);
+        } 
+        else {
+            l++;
+        }
+    }
+    return l + 1;
 }
+
+// console.log(removeElement([3,2,2,3], 3), 2); // removed indexes can hold anything
+console.log(removeElement([0,1,2,2,3,0,4,2], 2), 5); // removed indexes can hold anything
 
 
 
