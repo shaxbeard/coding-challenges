7ae3cf376bbf584e3f90de2fc86841a5c0f3ac7b
Eric Carlson <eric.carlson2972@gmail.com>
2024-10-18 12:33:35 -0400

    add constant time solution with gauss formula

--- sum-mixed-array-8arr.js
+++ sum-mixed-array-8arr.js
@@ -10,13 +10,28 @@
 //Pseudocode -
 //Methods!
 
-function sumMix(arr) {
-  //Method #1 - the reduce() method with map(Number)
-  return arr.map(Number).reduce((sum, num) => sum + num, 0);
+// function sumMix(arr) {
+//   //Method #1 - the reduce() method with map(Number)
+//   return arr.map(Number).reduce((sum, num) => sum + num, 0);
 
-  //Method #2 - the reduce() method with the unary operator
-  return arr.reduce((sum, num) => sum + +num, 0);
+//   //Method #2 - the reduce() method with the unary operator
+//   return arr.reduce((sum, num) => sum + +num, 0);
+// }
+
+//Method #3  - Use gauss formula to sum the array
+function sumContiguousNums(arr) {
+  const numArr = arr.map(num => Number(num));
+  const firstNum = numArr[0];
+  const lastNum = numArr[numArr.length - 1];
+  const n = numArr.length;
+
+  // Gauss formula: Sum = n * (firstNum + lastNum) / 2
+  return (n * (firstNum + lastNum)) / 2;
 }
 
-console.log(sumMix([1, 2, "3", "4"]), 10);
-console.log(sumMix(["5", "0", 2, "3", "4", 6]), 20);
+console.log(sumContiguousNums([1, 2, "3", "4", 5]), 15);
+console.log(sumContiguousNums(["2", 3, "4", "5", 6]), 20);
+
+
+
+
